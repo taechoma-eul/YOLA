55a18cd1bf382603ce239ddaee07a80bffd110a9
SuimKim <rrrr6563@naver.com>
2025-04-01 10:59:05 +0900

    feat:#3 회원가입시 닉네임 메타데이터 삽입 로직 추가

--- src/app/(auth)/login/action.ts
+++ src/app/(auth)/login/action.ts
@@ -26,7 +26,12 @@ export async function signup(formData: FormData) {
   const supabase = await createClient();
   const data = {
     email: formData.get('email') as string,
-    password: formData.get('password') as string
+    password: formData.get('password') as string,
+    options: {
+      data: {
+        nickname: formData.get('nickname') as string
+      }
+    }
   };
 
   const { error } = await supabase.auth.signUp(data);
