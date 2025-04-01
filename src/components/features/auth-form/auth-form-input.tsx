fc77e6313a109c1e1171029e06f32acee2a177d5
SuimKim <rrrr6563@naver.com>
2025-04-02 02:44:14 +0900

    feat:#3 중복검사 로직 추가

--- src/components/features/auth-form/auth-form-input.tsx
+++ src/components/features/auth-form/auth-form-input.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import { useState } from 'react';
+import { supabase } from '@/lib/utils/supabase/supabase-client';
+import { SignupFormData } from '@/lib/utils/authValidate';
 import type { AuthInputProps } from '@/types/components/auth-form';
 
 const AuthFormInput = ({
@@ -8,8 +13,27 @@ const AuthFormInput = ({
   name,
   checkButton,
   errorMessage,
-  label
+  label,
+  getValues
 }: AuthInputProps) => {
+  const [checkErrorMessage, setCheckErrorMessage] = useState<string | null>(null);
+
+  const handleDuplicateCheck = async (field: keyof SignupFormData) => {
+    const value = getValues(field);
+    if (!value) {
+      setCheckErrorMessage(`${field === 'email' ? '이메일' : '닉네임'}을 먼저 입력해주세요`);
+      return;
+    } else if (errorMessage) return;
+
+    const { data } = await supabase.from('users').select(field).eq(field, value).single();
+    if (data) {
+      setCheckErrorMessage(`이미 사용 중인 ${field === 'email' ? '이메일' : '닉네임'}입니다`);
+    } else {
+      alert('사용 가능합니다');
+      setCheckErrorMessage(null);
+    }
+  };
+
   return (
     <div className="relative grid grid-cols-3 items-center gap-4">
       <label htmlFor={name} className="text-right">
@@ -24,13 +48,16 @@ const AuthFormInput = ({
       />
       {/* 레이아웃 시프트 때문에 일단 빈 버튼을 넣었습니다. 스타일적인 문제라 디자인 입힐 때 수정 예정입니다! */}
       {checkButton ? (
-        <button type="button" className="w-[100px] bg-gray-100 p-2">
+        <button type="button" className="w-[100px] bg-gray-100 p-2" onClick={() => handleDuplicateCheck(name)}>
           중복확인
         </button>
       ) : (
         <button></button>
       )}
       {errorMessage && <p className="absolute left-[280px] top-[45px] text-sm text-red-500">{errorMessage}</p>}
+      {checkErrorMessage && (
+        <p className="absolute left-[280px] top-[45px] text-sm text-red-500">{checkErrorMessage}</p>
+      )}
     </div>
   );
 };
