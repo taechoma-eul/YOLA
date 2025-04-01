b3dee6e7bb0f014655c2a840a55d3ed086eea2f4
ye21iin <kathykr120@gmail.com>
2025-04-01 20:24:18 +0900

    feat:#5 체크리스트 컴포넌트 구조 변경(메인 이동)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <></>;
+import { getUniqueMissionType } from '@/lib/utils/api/checklist.api';
+import ChecklistClient from '../components/features/checklist/checklist-client';
+
+export default async function Home() {
+  const uniqueTypes = await getUniqueMissionType();
+  return (
+    <>
+      <ChecklistClient uniqueTypes={uniqueTypes} />
+    </>
+  );
 }
