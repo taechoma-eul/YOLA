ab8ee38afe99c3d6d91e88580d552219c96189a8
SuimKim <rrrr6563@naver.com>
2025-04-02 03:11:46 +0900

    refactor:#3 변경된 컴포넌트로 적용

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,10 @@
-import AuthForm from '@/components/features/auth-form/auth-form';
-import { MODE } from '@/constants/auth-form';
+import LoginForm from '@/components/features/auth-form/login-form';
 
 const LoginPage = async () => {
   return (
     <div className="mx-auto w-[800px]">
       <h1 className="mb-6 text-center text-2xl font-bold">로그인</h1>
-      <AuthForm mode={MODE.LOGIN} />
+      <LoginForm />
     </div>
   );
 };
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,11 +1,10 @@
-import AuthForm from '@/components/features/auth-form/auth-form';
-import { MODE } from '@/constants/auth-form';
+import SignupForm from '@/components/features/auth-form/signup-form';
 
 export default function SignUpPage() {
   return (
     <div className="mx-auto w-[800px]">
       <h1 className="mb-6 text-center text-2xl font-bold">회원가입</h1>
-      <AuthForm mode={MODE.SIGNUP} />
+      <SignupForm />
     </div>
   );
 }
