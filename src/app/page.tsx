7299b9b0314f55901d2f3b906311d6beff7d5b3d
SuimKim <rrrr6563@naver.com>
2025-04-01 20:17:52 +0900

    test:#3 session 상태 확인을 위한 테스트 코드 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <></>;
-}
+import { createClient } from '@/lib/utils/supabase/supabase-server';
+
+const HomePage = async () => {
+  const supabase = await createClient();
+
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
+  console.log('user', user?.user_metadata);
+
+  return <div>HomePage</div>;
+};
+export default HomePage;
