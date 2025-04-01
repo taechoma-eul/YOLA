ba7f5794a92db66fc215d6dfb72397f6650172e8
Jieun Lee <2ur.leeje@gmail.com>
2025-04-01 21:06:50 +0900

    chore:#6 내 공감 게시글 보기 페이지 Path에 추가

--- src/constants/page-path.ts
+++ src/constants/page-path.ts
@@ -5,6 +5,7 @@ export const PATH = {
   MYPAGE: '/mypage/profile',
   MY_ACHIEVEMENT: '/mypage/achievement',
   MY_LIFE_LIST: '/mypage/my-life-list',
+  MY_GONGGAM_POST_LIST: '/mypage/my-gonggam-post-list',
   LIFE: '/life',
   LIFE_POST: '/life/post',
   CHECKLIST: '/checklist'
