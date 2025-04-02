04ad076a7a707111b1634e54d6028904ccf59a8a
SuimKim <rrrr6563@naver.com>
2025-04-02 09:46:56 +0900

    refactor:#3 비밀번호 조건 변경

--- src/lib/utils/authValidate.ts
+++ src/lib/utils/authValidate.ts
@@ -9,10 +9,10 @@ import { z } from 'zod';
 export const signupSchema = z
   .object({
     email: z.string({ required_error: '이메일을 입력해주세요.' }).email('유효한 이메일을 입력하세요.'),
-    password: z.string({ required_error: '비밀번호를 입력해주세요.' }).min(6, '비밀번호는 최소 6자 이상이어야 합니다.'),
+    password: z.string({ required_error: '비밀번호를 입력해주세요.' }).min(8, '비밀번호는 최소 8자 이상이어야 합니다.'),
     confirmPassword: z
       .string({ required_error: '비밀번호 확인을 입력해주세요.' })
-      .min(6, '비밀번호 확인은 최소 6자 이상이어야 합니다.'),
+      .min(8, '비밀번호 확인은 최소 8자 이상이어야 합니다.'),
     nickname: z
       .string({ required_error: '닉네임을 입력해주세요.' })
       .min(2, '닉네임은 최소 2자 이상이어야 합니다.')
