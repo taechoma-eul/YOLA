183fc622e849bf53caa8860a71fa94a193d9deac
SuimKim <rrrr6563@naver.com>
2025-04-02 15:33:15 +0900

    del:#3 테스트 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import { getUserMetadata } from '@/lib/utils/api/auth-action';
 import { getUniqueMissionType } from '@/lib/utils/api/checklist.api';
 import ChecklistClient from '@/components/features/checklist/checklist-client';
 
 const HomePage = async () => {
   const uniqueTypes = await getUniqueMissionType();
-  const user = await getUserMetadata();
-  console.log('user', user);
 
   return (
     <>
       <ChecklistClient uniqueTypes={uniqueTypes} />
     </>
   );
-}
+};
+
+export default HomePage;
