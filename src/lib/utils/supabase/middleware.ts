eaef4f5a60fa42e8aa5d1893f01ac8b3ee62fb94
SuimKim <rrrr6563@naver.com>
2025-04-02 16:14:23 +0900

    refactor:#3 미들웨어 리팩토링 - 경로 상수화 - 불필요한 주석 삭제

--- src/lib/utils/supabase/middleware.ts
+++ src/lib/utils/supabase/middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse, type NextRequest } from 'next/server';
 import { createClient } from '@/lib/utils/supabase/supabase-server';
+import { PATH } from '@/constants/page-path';
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
@@ -12,10 +13,9 @@ export async function updateSession(request: NextRequest) {
     data: { user }
   } = await supabase.auth.getUser();
 
-  if (!user && !request.nextUrl.pathname.startsWith('/login') && !request.nextUrl.pathname.startsWith('/auth')) {
-    // no user, potentially respond by redirecting the user to the login page
+  if (!user && !request.nextUrl.pathname.startsWith(PATH.LOGIN) && !request.nextUrl.pathname.startsWith(PATH.SIGNUP)) {
     const url = request.nextUrl.clone();
-    url.pathname = '/login';
+    url.pathname = PATH.LOGIN;
     return NextResponse.redirect(url);
   }
 
