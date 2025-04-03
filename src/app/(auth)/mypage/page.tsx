ce75401662f50b910cc50ac423e7c274c1b807b4
SuimKim <rrrr6563@naver.com>
2025-04-03 15:57:36 +0900

    feat:#32 프로필 수정 폼 컴포넌트 적용 - 파일 위치 변경

---
+++ src/app/(auth)/mypage/page.tsx
@@ -0,0 +1,12 @@
+import EditProfileForm from '@/components/features/mypage/edit-profile-form';
+
+const MyPage = () => {
+  return (
+    <>
+      <h1 className="my-10 text-xl font-bold">프로필 수정</h1>
+      <EditProfileForm />
+    </>
+  );
+};
+
+export default MyPage;
