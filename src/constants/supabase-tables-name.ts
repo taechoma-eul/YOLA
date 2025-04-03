7a3a4189aabe2b8ee9b1c146f5327bd2cb8a16f3
smileeeeely <minjoole.ivy@gmail.com>
2025-04-03 12:38:58 +0900

    chore:#19 수퍼베이스 테이블명 상수화

---
+++ src/constants/supabase-tables-name.ts
@@ -0,0 +1,12 @@
+export const TABLE = {
+  COMMENTS: 'comments',
+  GONGGAM_POST_IMAGE_PATH: 'gonggam_post_image_path',
+  GONGGAM_POST_TAGS: 'gonggam_post_tags',
+  GONGGAM_POSTS: 'gonggam_posts',
+  LIFE_POST_IMAGE_PATH: 'life_post_image_path',
+  LIFE_POSTS: 'life_posts',
+  LIKES: 'likes',
+  MISSION_LIST: 'mission_list',
+  USER_MISSION: 'user_mission',
+  USERS: 'users'
+};
--- src/lib/utils/api/mission-list.api.ts
+++ src/lib/utils/api/mission-list.api.ts
@@ -1,11 +1,12 @@
 'use server';
 
+import { TABLE } from '@/constants/supabase-tables-name';
 import { createClient } from '@/lib/utils/supabase/supabase-server';
 
 // 전체 미션 데이터 중 랜덤으로 하나씩 반환합니다
 export const getRandomMissionData = async (): Promise<RandomMissionData> => {
   const supabase = await createClient();
-  const { data, error } = await supabase.from('mission_list').select('*');
+  const { data, error } = await supabase.from(TABLE.MISSION_LIST).select('*');
   if (!data) {
     throw new Error('요청한 데이터가 없습니다.');
   }
