a6f9d38fd86fb47900093392602e70c9a3c7a8b7
UrePu <darkin31313@gmail.com>
2025-04-04 13:48:54 +0900

    refactor: cli 사용에 따른 type 변경

--- src/types/life-post.ts
+++ src/types/life-post.ts
@@ -1,10 +1,6 @@
-export interface LifePost {
-  id: string;
-  created_at: string;
-  user_id: string;
-  content: string;
-  mission_id: string | null;
-}
+import type { Database } from './supabase';
+
+export type LifePost = Database['public']['Tables']['life_posts']['Row'];
 export interface SoloLifeCardType {
   id: string;
   date: string;
