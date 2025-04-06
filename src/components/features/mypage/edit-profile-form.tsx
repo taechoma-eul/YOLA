7f8ac6e0dd3a902375c262221c9f61dd105940d6
SuimKim <rrrr6563@naver.com>
2025-04-06 16:44:58 +0900

    feat:#32 프로필 수정 폼 컴포넌트 및 로직 구현

--- src/components/features/mypage/edit-profile-form.tsx
+++ src/components/features/mypage/edit-profile-form.tsx
@@ -1,59 +1,68 @@
 'use client';
-import { z } from 'zod';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+
+import { Form, FormField } from '@/components/ui/form';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import DEFAULT_AVATAR from '@images/images/default-avatar.png';
-import Image from 'next/image';
-
-const formSchema = z.object({
-  nickname: z
-    .string({ required_error: '닉네임을 입력해주세요.' })
-    .min(2, '닉네임은 최소 2자 이상이어야 합니다.')
-    .max(10, '닉네임은 10자 이하여야 합니다.')
-});
+import { useUserProfile } from '@/lib/hooks/queries/use-get-user-profile';
+import { useUpdateProfileMutate } from '@/lib/hooks/mutations/use-profile-update-mutate';
+import { editProfileSchema } from '@/lib/utils/validation/auth-validate';
+import { profileImageUpload } from '@/lib/utils/api/profile-image-upload.api';
+import ProfileImageField from './edit-profile-form-image-field';
+import { EditFormData } from '@/types/components/edit-profile-form';
+import ProfileEmailField from './edit-profile-form-email-field';
+import NicknameField from './edit-profile-form-nickname-field';
 
 const EditProfileForm = () => {
-  const form = useForm({
-    resolver: zodResolver(formSchema),
+  const { profile, isProfilePending, isProfileError } = useUserProfile();
+  const updateProfile = useUpdateProfileMutate();
+
+  const form = useForm<EditFormData>({
+    resolver: zodResolver(editProfileSchema),
     defaultValues: {
-      nickname: ''
+      nickname: '',
+      profile_image_file: null,
+      profile_image: ''
     }
   });
+
+  const handleProfileImageUpload = async () => {
+    const file = form.getValues('profile_image_file')?.[0];
+
+    if (!file) return null;
+
+    const newImageUrl = await profileImageUpload(file, profile);
+    return newImageUrl;
+  };
+
+  const handleUpdateProfile = async (formData: EditFormData) => {
+    try {
+      const imageUrl = await handleProfileImageUpload();
+      const updatedData = {
+        ...formData,
+        profile_image: imageUrl || formData.profile_image // 업로드 없으면 기존 값 유지
+      };
+      updateProfile(updatedData);
+    } catch (error) {
+      console.log('사용자 정보 변경에 실패했습니다.');
+    }
+  };
+
+  if (isProfileError || isProfilePending || !profile) return;
+
   return (
     <Form {...form}>
-      <form className="w-[500px] space-y-5 border-2 p-5">
-        <div className="mx-auto mb-10 flex w-[150px] flex-col justify-center gap-5">
-          <figure>
-            <Image src={DEFAULT_AVATAR} alt="프로필 이미지" width={150} height={150} className="rounded-full border" />
-          </figure>
-          <Button>이미지 변경</Button>
-        </div>
-        <div className="flex items-center space-x-5">
-          <p className="w-20 text-end text-lg">이메일</p>
-          <div className="flex-1">
-            <p className="mb-3 text-lg">suim0215@gmail.com</p>
-          </div>
-        </div>
+      <form className="w-[500px] space-y-5 border-2 p-5" onSubmit={form.handleSubmit(handleUpdateProfile)}>
+        <FormField
+          control={form.control}
+          name="profile_image"
+          render={() => <ProfileImageField form={form} profileImage={profile.profile_image} />}
+        />
+        <ProfileEmailField email={profile.email} />
         <FormField
           control={form.control}
           name="nickname"
-          render={({ field }) => (
-            <FormItem className="flex items-center space-x-5">
-              <FormLabel className="w-20 text-end text-lg">닉네임</FormLabel>
-              <div className="flex flex-1 items-center space-x-3">
-                <div className="flex-1">
-                  <FormControl>
-                    <Input className="w-full" placeholder="닉네임 입력" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </div>
-                <Button type="button">중복확인</Button>
-              </div>
-            </FormItem>
-          )}
+          render={({ field }) => <NicknameField field={field} nickname={profile.nickname ? profile.nickname : ''} />}
         />
         <div className="h-[30px]" />
         <Button type="submit" className="w-full">
