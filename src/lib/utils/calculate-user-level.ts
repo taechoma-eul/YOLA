0c4cc6260b7708139e5001e5239a4d31fb044b74
Jieun Lee <2ur.leeje@gmail.com>
2025-04-06 11:40:28 +0900

    refactor:#6 switch문 if문으로 리팩토링

--- src/lib/utils/calculate-user-level.ts
+++ src/lib/utils/calculate-user-level.ts
@@ -5,29 +5,20 @@
  * @returns - 시작 단계, 입문 단계, 초보 단계, 중수 단계, 고수 단계, 마스터 단계
  */
 export const calculateUserLevel = (totalCount: number): string => {
-  switch (true) {
-    case totalCount >= 0 && totalCount < 25:
-      return USER_LEVELS.START;
-    case totalCount >= 25 && totalCount < 50:
-      return USER_LEVELS.BEGINNER;
-    case totalCount >= 50 && totalCount < 75:
-      return USER_LEVELS.NOVICE;
-    case totalCount >= 75 && totalCount < 100:
-      return USER_LEVELS.INTERMEDIATE;
-    case totalCount >= 100 && totalCount < 125:
-      return USER_LEVELS.EXPERT;
-    case totalCount >= 125:
-      return USER_LEVELS.MASTER;
-    default:
-      return '알 수 없음'; // 예외 처리
-  }
+  if (totalCount >= 125) return USER_LEVELS.MASTER;
+  if (totalCount >= 100) return USER_LEVELS.EXPERT;
+  if (totalCount >= 75) return USER_LEVELS.INTERMEDIATE;
+  if (totalCount >= 50) return USER_LEVELS.NOVICE;
+  if (totalCount >= 25) return USER_LEVELS.BEGINNER;
+  if (totalCount >= 0) return USER_LEVELS.START;
+  return '알 수 없음';
 };
 
 export const USER_LEVELS = {
-  START: '시작 단계',
-  BEGINNER: '입문 단계',
-  NOVICE: '초보 단계',
-  INTERMEDIATE: '중수 단계',
-  EXPERT: '고수 단계',
-  MASTER: '마스터 단계'
+  START: '',
+  BEGINNER: '입문',
+  NOVICE: '초보',
+  INTERMEDIATE: '중수',
+  EXPERT: '고수',
+  MASTER: '마스터'
 } as const;
