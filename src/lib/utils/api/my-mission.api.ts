e51f9ae1aa0e269ceed657844f11b9423864a774
Jieun Lee <2ur.leeje@gmail.com>
2025-04-06 13:57:09 +0900

    refactor:#6 api에서 로그인한 유저 Id 조회 함수 -> getUserId로 변경 적용

--- src/lib/utils/api/my-mission.api.ts
+++ src/lib/utils/api/my-mission.api.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@/lib/utils/supabase/supabase-server';
+import { getUserId } from './auth-action';
 
 /**
  * Supabase에서 `user_mission` 테이블의 `로그인 한 User`가 달성한 미션 전부를 조회하는 함수
@@ -9,18 +10,15 @@ import { createClient } from '@/lib/utils/supabase/supabase-server';
  */
 export const getUserMission = async () => {
   const supabase = await createClient();
-  //로그인한 유저 id 조회 (임시용)
-  const {
-    data: { user },
-    error: userError
-  } = await supabase.auth.getUser();
-  if (!user) throw userError;
+  //로그인한 유저 id 조회
+  const userId = await getUserId();
+  if (!userId) throw new Error('로그인 한 유저의 아이디가 존재하지 않습니다');
 
   // 로그인한 유저의 완료 미션과, mission_list 테이블에서 id와 type을 조인하여 조회
   const { data: UserMissions, error } = await supabase
     .from('user_mission')
     .select(`*,mission_list(type, level)`)
-    .eq('user_id', user.id);
+    .eq('user_id', userId);
 
   if (error) throw new Error(error.message);
   return UserMissions;
@@ -35,15 +33,12 @@ export const getUserMission = async () => {
  */
 export const getUserMissionLevels = async () => {
   const supabase = await createClient();
-  //로그인한 유저 id 조회 (임시용)
-  const {
-    data: { user },
-    error: userError
-  } = await supabase.auth.getUser();
-  if (!user) throw userError;
+  //로그인한 유저 id 조회
+  const userId = await getUserId();
+  if (!userId) throw new Error('로그인 한 유저의 아이디가 존재하지 않습니다');
 
   // 로그인한 유저의 레벨 조회
-  const { data: UserMissionsLevels, error } = await supabase.from('user_level').select('*').eq('user_id', user.id);
+  const { data: UserMissionsLevels, error } = await supabase.from('user_level').select('*').eq('user_id', userId);
 
   if (error) throw new Error(error.message);
   return UserMissionsLevels[0];
