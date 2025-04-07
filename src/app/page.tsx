1724ad769052d0b8e69f21c9ab9452df967a1f6f
SuimKim <rrrr6563@naver.com>
2025-04-07 12:30:45 +0900

    feat:#47 공감게시판 컴포넌트 적용 및 스타일 조정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import { getUniqueMissionType } from '@/lib/utils/api/checklist.api';
 import MainBannerSwiper from '@/components/features/home/main-banner-swiper';
 import CheckListCard from '@/components/features/home/main-checklist-card';
 import RandomMissionBox from '@/components/features/home/main-random-mission-box';
+import GonggamPreviewBox from '@/components/features/home/main-gonggam-preview';
 
 const HomePage = async () => {
   const uniqueTypes = await getUniqueMissionType();
 
-  console.log('uniqueTypes', uniqueTypes);
   return (
     <div className="space-y-14">
       <MainBannerSwiper />
@@ -19,9 +19,10 @@ const HomePage = async () => {
         </div>
       </section>
       <hr className="mx-auto w-full max-w-[1280px] outline-neutral-300" />
-      <section className="relative flex">
+      <section className="relative grid max-w-[1280px] grid-cols-2 place-content-evenly items-center justify-items-center gap-4">
         <RandomMissionBox />
-        <div className="absolute -top-2 left-[50%] h-72 w-0 outline outline-1 outline-neutral-200" />
+        <div className="absolute left-[50%] h-72 w-0 outline outline-1 outline-neutral-200" />
+        <GonggamPreviewBox />
       </section>
     </div>
   );
