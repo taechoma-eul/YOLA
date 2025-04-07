cebc98290d992f5b48958db452f4f63d2a0bf3a9
SuimKim <rrrr6563@naver.com>
2025-04-07 12:29:51 +0900

    refactor:#47 랜덤 미션 컴포넌트 세부 컴포넌트 분리

--- src/components/features/home/main-random-mission-box.tsx
+++ src/components/features/home/main-random-mission-box.tsx
@@ -1,19 +1,16 @@
+import { Button } from '@/components/ui/button';
+import RandomMissionContainer from '@/components/features/home/main-random-mission-container';
+import TitleBox from '@/components/features/home/main-random-title-box';
+
 const RandomMissionBox = () => {
   return (
-    <div className="inline-flex items-center justify-start gap-16">
-      <div className="inline-flex w-56 flex-col items-start justify-start gap-24">
-        <div className="flex flex-col items-start justify-start gap-8 self-stretch">
-          <div className="h-5 justify-start self-stretch text-xl font-semibold text-black">오늘의 랜덤 미션</div>
-          <div className="justify-start self-stretch text-sm font-normal leading-tight text-black">
-            혼자서 보내는 시간이 지루하게 느껴질 때, 다양한 주제의 랜덤 미션을 통해 매일 색다른 하루를 만들어보세요.
-          </div>
-        </div>
-        <div className="inline-flex items-center justify-center gap-2.5 self-stretch rounded p-2.5 outline outline-1 outline-offset-[-1px] outline-stone-300">
-          <div className="justify-start text-xs font-normal leading-tight text-black">랜덤미션 뽑기</div>
-        </div>
+    <RandomMissionContainer>
+      <div className="inline-flex w-60 flex-col items-start justify-start gap-24">
+        <TitleBox />
+        <Button className="w-full border-2 bg-white text-xs text-black shadow-none">랜덤미션 뽑기</Button>
       </div>
-      <div className="h-64 w-56 bg-zinc-300" />
-    </div>
+      <div className="h-64 w-56 bg-zinc-300">뽑기 이미지</div>
+    </RandomMissionContainer>
   );
 };
 
---
+++ src/components/features/home/main-random-mission-container.tsx
@@ -0,0 +1,7 @@
+import { Children } from '@/types/children';
+
+const RandomMissionContainer = ({ children }: Children) => {
+  return <div className="inline-flex items-center justify-start gap-12">{children}</div>;
+};
+
+export default RandomMissionContainer;
