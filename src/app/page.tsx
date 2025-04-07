607b7e9f636169826d24a1c1bed2e557ac2b30ed
SuimKim <rrrr6563@naver.com>
2025-04-07 12:58:06 +0900

    feat:#47 모달 연결 - 로그인 상태 확인 로직 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import MainBannerSwiper from '@/components/features/home/main-banner-swiper';
 import CheckListCard from '@/components/features/home/main-checklist-card';
 import RandomMissionBox from '@/components/features/home/main-random-mission-box';
 import GonggamPreviewBox from '@/components/features/home/main-gonggam-preview';
+import { getUserSessionState } from '@/lib/utils/api/auth-action';
 
 const HomePage = async () => {
-  const uniqueTypes = await getUniqueMissionType();
+  const checkListType = await getUniqueMissionType();
+  const { isLogin } = await getUserSessionState();
 
   return (
     <div className="space-y-14">
@@ -13,14 +15,14 @@ const HomePage = async () => {
       <section className="space-y-5">
         <strong className="text-xl">YOLA 챌린지</strong>
         <div className="flex gap-10">
-          {uniqueTypes.map((item, index) => (
-            <CheckListCard key={index} checkListType={item} />
+          {checkListType.map((item) => (
+            <CheckListCard key={item} checkListType={item} />
           ))}
         </div>
       </section>
       <hr className="mx-auto w-full max-w-[1280px] outline-neutral-300" />
       <section className="relative grid max-w-[1280px] grid-cols-2 place-content-evenly items-center justify-items-center gap-4">
-        <RandomMissionBox />
+        <RandomMissionBox isLogin={isLogin} />
         <div className="absolute left-[50%] h-72 w-0 outline outline-1 outline-neutral-200" />
         <GonggamPreviewBox />
       </section>
--- src/components/features/home/main-random-mission-box.tsx
+++ src/components/features/home/main-random-mission-box.tsx
@@ -1,13 +1,23 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import RandomMissionContainer from '@/components/features/home/main-random-mission-container';
 import TitleBox from '@/components/features/home/main-random-title-box';
+import { useState } from 'react';
+import RandomMissionModal from '../modals/random-mission';
 
-const RandomMissionBox = () => {
+const RandomMissionBox = ({ isLogin }: { isLogin: boolean }) => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const clickModal = () => setShowModal(!showModal);
+
   return (
     <RandomMissionContainer>
       <div className="inline-flex w-60 flex-col items-start justify-start gap-24">
         <TitleBox />
-        <Button className="w-full border-2 bg-white text-xs text-black shadow-none">랜덤미션 뽑기</Button>
+        <Button onClick={clickModal} className="w-full border-2 bg-white text-xs text-black shadow-none">
+          랜덤미션 뽑기
+        </Button>
+        {showModal && <RandomMissionModal clickModal={clickModal} isLogin={isLogin} />}
       </div>
       <div className="h-64 w-56 bg-zinc-300">뽑기 이미지</div>
     </RandomMissionContainer>
