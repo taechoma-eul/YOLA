e4059aa5a50f58daf83eb392e1a686c8701f2452
UrePu <darkin31313@gmail.com>
2025-04-07 20:01:49 +0900

    del: get date 함수로 변경

--- src/lib/utils/get-today.ts
+++
@@ -1,7 +0,0 @@
-export const getToday = (): string => {
-  const kst = new Date();
-  const yyyy = kst.getFullYear();
-  const mm = String(kst.getMonth() + 1).padStart(2, '0');
-  const dd = String(kst.getDate()).padStart(2, '0');
-  return `${yyyy}-${mm}-${dd}`;
-};
