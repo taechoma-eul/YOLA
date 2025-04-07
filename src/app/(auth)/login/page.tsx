dc343b86135683e374ff768f76294cd82464335a
SuimKim <rrrr6563@naver.com>
2025-04-08 02:37:54 +0900

    feat:#66 반복 로직 공통 컴포넌트로 생성 및 적용

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,8 @@
-import LoginForm from '@/components/features/auth-form/login-form';
+import AuthFormViewUi from '@/components/features/auth-form/auth-page-ui';
+import { MODE } from '@/constants/auth-form';
 
 const LoginPage = async () => {
-  return (
-    <div className="mx-auto w-[800px]">
-      <h1 className="mb-6 text-center text-2xl font-bold">로그인</h1>
-      <LoginForm />
-    </div>
-  );
+  return <AuthFormViewUi mode={MODE.LOGIN} />;
 };
 
 export default LoginPage;
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,10 +1,8 @@
-import SignupForm from '@/components/features/auth-form/signup-form';
+import AuthFormViewUi from '@/components/features/auth-form/auth-page-ui';
+import { MODE } from '@/constants/auth-form';
 
-export default function SignUpPage() {
-  return (
-    <div className="mx-auto w-[800px]">
-      <h1 className="mb-6 text-center text-2xl font-bold">회원가입</h1>
-      <SignupForm />
-    </div>
-  );
-}
+const SignUpPage = () => {
+  return <AuthFormViewUi mode={MODE.SIGNUP} />;
+};
+
+export default SignUpPage;
