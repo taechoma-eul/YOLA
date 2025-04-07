f8e7023f186d069be586a0f7cf74ef0b3f62f57a
SuimKim <rrrr6563@naver.com>
2025-04-07 15:09:07 +0900

    feat:#59 supabase에서 받아온 데이터로 게시글 최신순 3개 렌더링

--- src/components/features/home/main-gonggam-preview-title-box.tsx
+++ src/components/features/home/main-gonggam-preview-title-box.tsx
@@ -1,8 +1,13 @@
+import { PATH } from '@/constants/page-path';
+import Link from 'next/link';
+
 const TitleBox = () => {
   return (
     <div className="mb-4 flex w-full items-end justify-between">
       <strong className="text-xl font-semibold">1인 가구 공감 게시판</strong>
-      <p className="text-sm font-normal text-zinc-500">전체보기</p>
+      <Link href={PATH.GONGGAM} className="text-sm font-normal text-zinc-500">
+        전체보기
+      </Link>
     </div>
   );
 };
--- src/components/features/home/main-gonggam-preview.tsx
+++ src/components/features/home/main-gonggam-preview.tsx
@@ -1,14 +1,19 @@
 import GonggamItem from '@/components/features/home/main-gonggam-item';
 import GonggamPreviewContainer from '@/components/features/home/main-gonggam-preview-container';
 import TitleBox from '@/components/features/home/main-gonggam-preview-title-box';
+import { getGonggamPosts } from '@/lib/utils/api/gonggam-posts.api';
 
-const GonggamPreviewBox = () => {
+const GonggamPreviewBox = async () => {
+  const gonggamPosts = await getGonggamPosts();
+
+  const previewList = [...gonggamPosts].slice(0, 3);
+
   return (
     <GonggamPreviewContainer>
       <TitleBox />
-      <GonggamItem />
-      <GonggamItem />
-      <GonggamItem />
+      {previewList.map((item) => (
+        <GonggamItem key={item.id} post={item} />
+      ))}
     </GonggamPreviewContainer>
   );
 };
