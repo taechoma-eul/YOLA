f02a57f5d01ef056b1a74653d05168415207ccee
Jieun Lee <2ur.leeje@gmail.com>
2025-04-07 19:44:35 +0900

    feat:#44 무한스크롤 API 조회할때 필요한 response 타입 정의

--- src/types/life-post.ts
+++ src/types/life-post.ts
@@ -10,9 +10,15 @@ export interface SoloLifeCardType {
   thumbnail: string; // 대표 이미지 (보통 첫 번째 이미지)
   imageUrls: string[]; // 전체 이미지 목록
   isMission: boolean;
-  tags: string[];
 }
 
 export interface LifePostWithImageUrls extends LifePost {
   image_urls: string[];
+}
+
+//페이지네이션으로 라이프포스트 조회할때 필요한 response 타입
+export interface GetLifePostsResponse {
+  data: LifePostWithImageUrls[];
+  page: number;
+  totalPages: number;
 }
