acde84992cc81475233a97b5018a097256eaf142
SuimKim <rrrr6563@naver.com>
2025-04-08 14:43:10 +0900

    refactor#66 zod 로직 재사용을 위한 nickname 분리

--- src/lib/utils/validation/auth-validate.ts
+++ src/lib/utils/validation/auth-validate.ts
@@ -6,6 +6,17 @@ import { z } from 'zod';
  * check password와 password가 일치하지 않을 때의 오류 메시지가 별도로 지정되어 있습니다.
  * z.infer로 스키마에서 타입을 자동 추론합니다.
  */
+const nicknameSchema = z
+  .string({ required_error: '닉네임을 입력해주세요.' })
+  .min(2, '최소 2자 이상이어야 합니다.')
+  .max(8, '최소 8자 이하여야 합니다.')
+  .regex(/^[가-힣A-Za-z0-9]+$/, '공백, 초성을 제외한 한글, 영어, 숫자만 사용 가능합니다.')
+  .refine((val) => !/^\d+$/.test(val), '문자와 숫자 조합으로 사용 가능합니다.');
+
+export const editProfileSchema = z.object({
+  nickname: nicknameSchema
+});
+
 export const authSchema = z
   .object({
     email: z.string({ required_error: '이메일을 입력해주세요.' }).email('유효한 이메일을 입력하세요.'),
@@ -17,12 +28,7 @@ export const authSchema = z
       .refine((val) => /[A-Z]/.test(val), '대문자를 포함해야 합니다.')
       .refine((val) => /[!@#$%^&*]/.test(val), '특수문자를 포함해야 합니다.'),
     checkPassword: z.string({ required_error: '비밀번호를 한번 더 입력해주세요.' }),
-    nickname: z
-      .string({ required_error: '닉네임을 입력해주세요.' })
-      .min(2, '최소 2자 이상이어야 합니다.')
-      .max(8, '최소 8자 이하여야 합니다.')
-      .regex(/^[가-힣A-Za-z0-9]+$/, '공백, 초성을 제외한 한글, 영어, 숫자만 사용 가능합니다.')
-      .refine((val) => !/^\d+$/.test(val), '문자와 숫자 조합으로 사용 가능합니다.')
+    nickname: nicknameSchema
   })
   .refine((data) => data.password === data.checkPassword, {
     message: '비밀번호가 일치하지 않습니다.',
