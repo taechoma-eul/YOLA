6b86a377ee5c4402835d7e2db87e3a98867d1753
ye21iin <kathykr120@gmail.com>
2025-04-08 14:33:42 +0900

    design:#74 GonggamHeader 컴포넌트 추가 - 게시판 제목 및 버튼 포함

--- src/app/gonggam/layout.tsx
+++ src/app/gonggam/layout.tsx
@@ -1,9 +1,10 @@
+import GonggamHeader from '@/components/features/gonggam/gonggam-header';
 import { TabsNav } from '@/components/features/gonggam/gonggam-tabs-nav';
 
 const GonggamLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-full p-8 pt-0">
-      <h1 className="mb-5 text-2xl font-extrabold">혼자 라이프 공감 게시판</h1>
+      <GonggamHeader />
       <TabsNav />
       <div className="mt-6">{children}</div>
     </div>
