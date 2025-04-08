5170002394f0b61170cd5bf525173a5da4994a0d
SuimKim <rrrr6563@naver.com>
2025-04-09 02:00:26 +0900

    refactor:#66 소셜 로그인 에러 처리 추가

--- src/components/features/auth-form/login-form-button.tsx
+++ src/components/features/auth-form/login-form-button.tsx
@@ -1,10 +1,19 @@
 import Link from 'next/link';
+import { signInWithGoogle, signInWithKakao } from '@/lib/utils/api/auth-action';
+import { toastAlert } from '@/lib/utils/toast';
 import { Button } from '@/components/ui/button';
 import type { FieldData } from '@/types/components/auth-form';
 import { PATH } from '@/constants/page-path';
-import { signInWithGoogle, signInWithKakao } from '@/lib/utils/api/auth-action';
 
 const LoginFormButton = ({ isSubmitting }: Pick<FieldData, 'isSubmitting'>) => {
+  const handleSignInWithOAuth = (signAction: () => Promise<never>) => {
+    try {
+      signAction();
+    } catch (error) {
+      toastAlert('소셜 로그인에 실패했습니다.', 'destructive');
+    }
+  };
+
   return (
     <>
       <div className="space-y-3 pt-3">
@@ -17,10 +26,10 @@ const LoginFormButton = ({ isSubmitting }: Pick<FieldData, 'isSubmitting'>) => {
       </div>
       <hr className="h-[30px] border-b border-t-0 border-gray-300" />
       <div className="flex gap-5">
-        <Button type="submit" className="h-[42px] w-full" formAction={signInWithGoogle}>
+        <Button type="submit" className="h-[42px] w-full" formAction={() => handleSignInWithOAuth(signInWithGoogle)}>
           구글
         </Button>
-        <Button type="submit" className="h-[42px] w-full" formAction={signInWithKakao}>
+        <Button type="submit" className="h-[42px] w-full" formAction={() => handleSignInWithOAuth(signInWithKakao)}>
           카카오
         </Button>
       </div>
