7fc7efd9ded05560d043d990f0fd0f9bc1e1bf21
ye21iin <kathykr120@gmail.com>
2025-04-09 00:12:46 +0900

    chore:#43 타입 수정(공통화 Children)

--- src/app/gonggam/(board)/[category]/layout.tsx
+++ src/app/gonggam/(board)/[category]/layout.tsx
@@ -1,7 +1,8 @@
 import GonggamHeader from '@/components/features/gonggam/gonggam-header';
 import { TabsNav } from '@/components/features/gonggam/gonggam-tabs-nav';
+import type { Children } from '@/types/children';
 
-const GonggamLayout = ({ children }: { children: React.ReactNode }) => {
+const GonggamLayout = ({ children }: Children) => {
   return (
     <div className="w-full p-8 pt-0">
       <GonggamHeader />
--- src/app/gonggam/(post)/[category]/[postId]/layout.tsx
+++ src/app/gonggam/(post)/[category]/[postId]/layout.tsx
@@ -1,4 +1,6 @@
-const PostDetailLayout = ({ children }: { children: React.ReactNode }) => {
+import type { Children } from '@/types/children';
+
+const PostDetailLayout = ({ children }: Children) => {
   return (
     <div className="w-full p-8 pt-0">
       <div className="mt-6">{children}</div>
