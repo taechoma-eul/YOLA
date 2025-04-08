dbf4b93c6446e10b77930456191a3fdd4fa49df6
ye21iin <kathykr120@gmail.com>
2025-04-08 14:18:00 +0900

    design:#74 레이아웃 스타일 수정 - 패딩 및 너비 조정

--- src/app/gonggam/layout.tsx
+++ src/app/gonggam/layout.tsx
@@ -2,7 +2,7 @@ import { TabsNav } from '@/components/features/gonggam/gonggam-tabs-nav';
 
 const GonggamLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="p-4">
+    <div className="w-full p-8 pt-0">
       <h1 className="mb-5 text-2xl font-extrabold">혼자 라이프 공감 게시판</h1>
       <TabsNav />
       <div className="mt-6">{children}</div>
