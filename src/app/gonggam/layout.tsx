df05a0bebde9d0516b35c363c0c46c1f422142dc
ye21iin <kathykr120@gmail.com>
2025-04-08 12:57:53 +0900

    design:#74 게시판 제목 추가

--- src/app/gonggam/layout.tsx
+++ src/app/gonggam/layout.tsx
@@ -3,6 +3,7 @@ import { TabsNav } from '@/components/features/gonggam/gonggam-tabs-nav';
 const GonggamLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="p-4">
+      <h1 className="mb-5 text-2xl font-extrabold">혼자 라이프 공감 게시판</h1>
       <TabsNav />
       <div className="mt-6">{children}</div>
     </div>
