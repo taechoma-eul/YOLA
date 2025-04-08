05fb4294fbb3124843a2d81e42f878cac8c87720
smileeeeely <minjoole.ivy@gmail.com>
2025-04-08 22:29:50 +0900

    chore:#22 주석 추가

--- src/components/common/edit-delete-dropdown.tsx
+++ src/components/common/edit-delete-dropdown.tsx
@@ -4,6 +4,10 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/compon
 
 const SET_TIME_OUT = 150;
 
+/**
+ * 수정/삭제를 할 수 있는 드롭다운입니다
+ * 수정 로직을 담은 함수와 삭제 로직을 담은 함수를 넣어주면 사용이 가능합니다
+ */
 const EditDeleteDropdown = ({ handleEdit, handleDelete }: { handleEdit: Function; handleDelete: Function }) => {
   const [isOpen, setIsopen] = useState<boolean>(false);
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
@@ -22,6 +26,7 @@ const EditDeleteDropdown = ({ handleEdit, handleDelete }: { handleEdit: Function
     }, SET_TIME_OUT);
   };
 
+  /** 메모리 누수 방지를 위한 클린업 함수 */
   useEffect(() => {
     return () => {
       if (timeoutRef.current) {
