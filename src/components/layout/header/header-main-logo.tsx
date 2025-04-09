9b382381f4f50fcea9f893c5100bbafe04684d0e
SuimKim <rrrr6563@naver.com>
2025-04-09 15:06:43 +0900

    refactor:#82 메인 로고 정적 이미지로 변경 - 이미지 경로 상수화 - link 태그 포함한 logo 컴포넌트 생성

---
+++ src/components/layout/header/header-main-logo.tsx
@@ -0,0 +1,14 @@
+import { MAIN_LOGO_URL } from '@/constants/default-image-url';
+import { PATH } from '@/constants/page-path';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const MainLogo = () => {
+  return (
+    <Link href={PATH.HOME}>
+      <Image src={MAIN_LOGO_URL} alt="YOLA 로고" width={83} height={23} />
+    </Link>
+  );
+};
+
+export default MainLogo;
--- src/constants/default-image-url.ts
+++ src/constants/default-image-url.ts
@@ -1,2 +1,3 @@
 export const DEFAULT_LIFE_IMAGE_URL = '/images/default-image.svg';
 export const DEFAULT_AVATAR_URL = '/images/default-avatar.svg';
+export const MAIN_LOGO_URL = '/images/main-logo.svg';
