a3ae2f7a1ad96f06519b05bb68bb540ca5811fe2
SuimKim <rrrr6563@naver.com>
2025-04-09 14:48:44 +0900

    fix:#82 드롭다운 메뉴 깜박거림 최소화 - mouse 이벤트를 상위 요소에 적용 후 감싸줌 - 글로벌 css 속성 추가 - setTimeout 사용하지 않고 스타일 조정으로 여백 메움

--- src/components/layout/header/header-dropdown-menu.tsx
+++ src/components/layout/header/header-dropdown-menu.tsx
@@ -14,34 +14,45 @@ interface MenuProps extends Children {
 const HeaderDropdownMenu = ({ menuItems, children, align }: MenuProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
+  const handleMouseEnter = () => {
+    setIsOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
-      <DropdownMenuTrigger
-        asChild
-        className="text-md font-normal focus:bg-transparent data-[state=open]:bg-transparent"
-        onMouseEnter={() => setIsOpen(true)} // hover 시 열기
-        onMouseLeave={() => setIsOpen(false)} // 떠나면 닫기
-      >
-        <button className="focus:outline-none">{children}</button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        align={align}
-        sideOffset={8}
-        className="inline-flex w-[140px] flex-col items-center gap-3 rounded-xl bg-white p-5 shadow-none"
-        onMouseEnter={() => setIsOpen(true)} // 드롭다운 메뉴에 마우스가 들어가면 유지
-        onMouseLeave={() => setIsOpen(false)} // 떠나면 닫기
-      >
-        {menuItems.map((item, index) => (
-          <HeaderDropdownMenuItem
-            key={index}
-            label={item.label}
-            path={item.path}
-            isLine={item.isLine}
-            isButton={item.isButton}
-          />
-        ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div
+      className="dropdown-wrapper relative inline-block"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <DropdownMenu open={isOpen}>
+        <DropdownMenuTrigger
+          asChild
+          className="text-md font-normal focus:bg-transparent data-[state=open]:bg-transparent"
+        >
+          <div className="">{children}</div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          align={align}
+          sideOffset={8}
+          className="inline-flex w-[140px] flex-col items-center gap-3 rounded-xl bg-white p-5 shadow-none"
+          style={{ pointerEvents: 'auto' }}
+        >
+          {menuItems.map((item, index) => (
+            <HeaderDropdownMenuItem
+              key={index}
+              label={item.label}
+              path={item.path}
+              isLine={item.isLine}
+              isButton={item.isButton}
+            />
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   );
 };
 
