5e4ef590e9389b10a11351df5100f68fc6cfa626
Jieun Lee <2ur.leeje@gmail.com>
2025-04-09 19:57:53 +0900

    feat:#85 마이공감게시글 response 타입 정의

--- src/types/gonggam-posts.ts
+++ src/types/gonggam-posts.ts
@@ -4,3 +4,10 @@ export interface GonggamPost extends Tables<'gonggam_posts'> {
   likes: Tables<'likes'>[];
   comments: Tables<'comments'>[];
 }
+
+//페이지네이션으로 나의 공감 게시글 조회할때 필요한 response 타입
+export interface GetMyGonggamPostsResponse {
+  data: GonggamPost[];
+  page: number;
+  totalPages: number;
+}
