aca35982dcc1cce7ad9f3bd64d3100984ab7034b
smileeeeely <minjoole.ivy@gmail.com>
2025-04-09 09:53:17 +0900

    chore:#22 쿼리키 및 수퍼베이스 테이블명 상수화 적용

--- src/lib/hooks/mutations/use-delete-life-post.ts
+++ src/lib/hooks/mutations/use-delete-life-post.ts
@@ -2,8 +2,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { supabase } from '@/lib/utils/supabase/supabase-client';
 import { TABLE } from '@/constants/supabase-tables-name';
 import type { LifePostWithImageUrls } from '@/types/life-post';
-
-const LIFE_POSTS_TABLE = TABLE.LIFE_POSTS;
+import { QUERY_KEY } from '@/constants/query-keys';
 
 export const useDeleteLifePost = () => {
   const queryClient = useQueryClient();
@@ -14,13 +13,13 @@ export const useDeleteLifePost = () => {
 
       // Step 1. 스토리지에서 이미지 삭제
       if (image_urls && image_urls.length > 0) {
-        const { error: storageError } = await supabase.storage.from('life_post_image_path').remove(image_urls);
+        const { error: storageError } = await supabase.storage.from(TABLE.LIFE_POST_IMAGE_PATH).remove(image_urls);
         if (storageError) throw new Error('스토리지 이미지 삭제 실패');
       }
 
       // Step 2. 이미지 경로 supabase 삭제
       const { error: imageDeleteError } = await supabase
-        .from('life_post_image_path')
+        .from(TABLE.LIFE_POST_IMAGE_PATH)
         .delete()
         .eq('life_post_id', postId);
       if (imageDeleteError) throw new Error('이미지 경로 supabase 삭제 실패');
@@ -28,7 +27,7 @@ export const useDeleteLifePost = () => {
       // Step 3. 유저 미션 삭제
       if (mission_id) {
         const { error: missionDeleteError } = await supabase
-          .from('user_mission')
+          .from(TABLE.USER_MISSION)
           .delete()
           .match({ user_id, completed_id: mission_id });
 
@@ -36,12 +35,12 @@ export const useDeleteLifePost = () => {
       }
 
       // Step 4. 게시글 삭제
-      const { error: postDeleteError } = await supabase.from(LIFE_POSTS_TABLE).delete().eq('id', postId);
+      const { error: postDeleteError } = await supabase.from(TABLE.LIFE_POSTS).delete().eq('id', postId);
       if (postDeleteError) throw new Error('게시글 삭제 실패');
     },
 
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['lifePosts'] });
+      queryClient.invalidateQueries({ queryKey: QUERY_KEY.LIFE_POSTS });
     }
   });
 };
