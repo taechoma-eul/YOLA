3448b42524fcb8ca3a42a6cf993fcf6ca2d9e4e3
Jieun Lee <2ur.leeje@gmail.com>
2025-04-10 20:08:28 +0900

    feat:85 마이공감포스트 타입추가

--- src/types/gonggam-posts.ts
+++ src/types/gonggam-posts.ts
@@ -1,4 +1,4 @@
-import { Tables } from '@/types/supabase';
+import { Database, Tables } from '@/types/supabase';
 
 export interface GonggamPost extends Tables<'gonggam_posts'> {
   likes: Tables<'likes'>[];
@@ -7,7 +7,19 @@ export interface GonggamPost extends Tables<'gonggam_posts'> {
 
 //페이지네이션으로 나의 공감 게시글 조회할때 필요한 response 타입
 export interface GetMyGonggamPostsResponse {
-  data: GonggamPost[];
+  data: GonggamPostWithCounts[];
   page: number;
   totalPages: number;
 }
+
+//gonggam_posts_with_counts 테이블 타입 정의
+export type GonggamPostWithCounts = {
+  id: number;
+  category: Database['public']['Enums']['categorys'];
+  content: string | null;
+  created_at: string;
+  updated_at: string;
+  user_id: string;
+  like_count: number | null;
+  comment_count: number | null;
+};
