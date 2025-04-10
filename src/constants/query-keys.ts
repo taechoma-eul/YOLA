167cec4074a2ee694b56726726ce8cf3841d6835
Jieun Lee <2ur.leeje@gmail.com>
2025-04-10 20:06:54 +0900

    feat:85 마이공감포스트 쿼리키 상수 추가

--- src/constants/query-keys.ts
+++ src/constants/query-keys.ts
@@ -2,5 +2,5 @@ export const QUERY_KEY = {
   PROFILE: ['profile'],
   LIFE_POSTS_INFINITE: ['life-posts-infinite'],
   LIFE_POSTS: ['lifePosts'],
-  GONGGAM_POSTS_INFINITE: ['gonggam-posts-infinite']
+  GONGGAM_POSTS_INFINITE: (sortBy: string) => ['gonggam-posts-infinite', sortBy]
 };
