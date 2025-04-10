dae8017c1689d62f3c8451361cb060bb35f1d72f
smileeeeely <minjoole.ivy@gmail.com>
2025-04-10 15:57:40 +0900

    refactor:#19 서버액션 -> 라우트핸들러

--- src/components/features/modals/random-mission.tsx
+++ src/components/features/modals/random-mission.tsx
@@ -1,6 +1,6 @@
-import { getRandomMissionData } from '@/lib/utils/api/mission-list.api';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { X } from 'lucide-react';
+import { useMissions } from '@/lib/hooks/queries/use-missions';
 
 /*
 사용할 곳에서
@@ -12,40 +12,65 @@ const clickModal = () => setShowModal(!showModal);
 을 통해 모달을 끌 수 있도록 setter 함수와 로그인 여부를 알려주는 isLogin을 넘겨주면 사용할 수 있습니다
  */
 const RandomMissionModal = ({ clickModal, isLogin }: { clickModal: Function; isLogin: boolean }) => {
-  const [randomMission, setRandomMission] = useState<string>('오늘의 랜덤 미션은?');
+  const [randomMission, setRandomMission] = useState<string>('');
 
-  useEffect(() => {
-    handleRandomMission();
-  }, []);
+  const { data: missionsData, isPending, isError } = useMissions();
 
-  const handleRandomMission = async () => {
-    try {
-      const randomMissionData = await getRandomMissionData();
-      setRandomMission(randomMissionData.content);
-    } catch (error) {
-      alert('미션을 가져오는 데 실패했습니다.');
-    }
+  if (isPending) {
+    return <div>...불러오는 중</div>;
+  }
+  if (isError) {
+    return <div>문제가 생겼어요!</div>;
+  }
+  const handleRandomMission = () => {
+    const randomIndex = Math.floor(Math.random() * missionsData.length);
+    const randomMissionData = missionsData[randomIndex];
+    setRandomMission(randomMissionData.content);
   };
 
   return (
     <div
       onClick={() => clickModal()} // 배경 클릭하면 모달 나가기
-      className="fixed top-0 flex h-full w-full items-center justify-center bg-gray-500/50"
+      className="fixed left-0 top-0 z-[51] flex h-full w-full items-center justify-center bg-black/60"
     >
       <div
         onClick={(e) => e.stopPropagation()} // 모달 안을 클릭 했을 때 나가는 것 막음
-        className="flex w-96 flex-col items-center justify-center rounded-2xl bg-slate-300 p-6"
+        className="relative h-96 w-[598px] overflow-hidden rounded-[20px] bg-white"
       >
-        <X onClick={() => clickModal()} className="cursor-pointer" />
-        <h1>오늘의 랜덤 미션</h1>
-        {/**TODO - 사진 / 로딩 스피너 삽입 */}
-        {isLogin ? <p>두근두근 뭐가 나올까?</p> : <p>더 많은 미션을 원하시면 회원가입하세요!</p>}
-        <div className="flex flex-col items-center justify-center gap-2">{randomMission}</div>
-        <div className="flex gap-8 pt-3">
-          <button onClick={() => clickModal()}>닫기</button>
-          <button type="button" onClick={() => handleRandomMission()}>
-            다시 뽑기
-          </button>
+        <div className="absolute right-[30px] top-[30px] z-[52] h-7 w-7 cursor-pointer">
+          <X onClick={() => clickModal()} className="h-full w-full" />
+        </div>
+        <div className="absolute left-[68px] top-[60px] inline-flex h-80 w-[462px] flex-col items-center justify-center gap-5">
+          <div className="flex flex-col items-center justify-start gap-2.5 self-stretch">
+            <div className="justify-start self-stretch text-center font-['Pretendard'] text-2xl font-semibold leading-loose text-zinc-800">
+              오늘의 랜덤 미션
+            </div>
+            <div className="justify-start self-stretch text-center font-['Pretendard'] text-base font-normal leading-snug text-zinc-800">
+              {isLogin ? <p>두근두근 뭐가 나올까?</p> : <p>더 많은 미션을 원하시면 회원가입하세요!</p>}
+            </div>
+            {/**TODO - 사진 / 로딩 스피너 삽입 */}
+          </div>
+          <div className="relative h-44 w-[462px] rounded-xl">
+            {randomMission ? (
+              <div className="flex flex-col items-center justify-center gap-2">{randomMission}</div>
+            ) : (
+              <div className="absolute left-0 top-0 h-44 w-[462px] rounded-2xl bg-gray-200" />
+            )}
+          </div>
+          <div
+            data-priority="Primary"
+            data-size="Medium"
+            data-status="Default"
+            className="pointer-cursor inline-flex w-56 items-center justify-center gap-2.5 rounded-xl bg-amber-300 px-4 py-2.5"
+            onClick={() => handleRandomMission()}
+          >
+            <button
+              type="button"
+              className="justify-start font-['Pretendard'] text-base font-semibold leading-snug text-zinc-800"
+            >
+              뽑기
+            </button>
+          </div>
         </div>
       </div>
     </div>
