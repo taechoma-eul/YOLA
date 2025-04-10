582329f0a1e6c4a759c4d823c35ccdef7bb9538a
SuimKim <rrrr6563@naver.com>
2025-04-11 02:11:31 +0900

    refactor:#99 로그인 중 알림을 위해 useTransition 훅 추가 사용

--- src/components/features/auth-form/login-form-button.tsx
+++ src/components/features/auth-form/login-form-button.tsx
@@ -1,29 +1,33 @@
 'use client';
 
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { toastAlert } from '@/lib/utils/toast';
 import { Button } from '@/components/ui/button';
 import type { FieldData } from '@/types/components/auth-form';
 import { PATH } from '@/constants/page-path';
-import { useRouter } from 'next/navigation';
 import { API } from '@/constants/api-path';
+import { useTransition } from 'react';
 
 const LoginFormButton = ({ isSubmitting }: Pick<FieldData, 'isSubmitting'>) => {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
   const handleSignInWithOAuth = (apiPath: string) => {
-    try {
-      router.push(apiPath);
-    } catch (error) {
-      toastAlert('소셜 로그인에 실패했습니다.', 'destructive');
-    }
+    startTransition(() => {
+      try {
+        router.push(apiPath);
+      } catch (error) {
+        toastAlert('소셜 로그인에 실패했습니다.', 'destructive');
+      }
+    });
   };
-
+  console.log('isPending', isPending);
   return (
     <>
       <div className="space-y-3 pt-3">
-        <Button type="submit" className="h-[42px] w-full">
-          {!isSubmitting ? '이메일로 로그인' : '로그인 중...'}
+        <Button disabled={isSubmitting || isPending ? true : false} type="submit" className="h-[42px] w-full">
+          {isSubmitting || !isPending ? '이메일로 로그인' : '로그인 중...'}
         </Button>
         <Button asChild className="h-[42px] w-full">
           <Link href={PATH.SIGNUP}>회원가입</Link>
