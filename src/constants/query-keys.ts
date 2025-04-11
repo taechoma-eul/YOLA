772f9651c3820db37749f08c128e3afa0175b3d7
UrePu <darkin31313@gmail.com>
2025-04-11 10:05:37 +0900

    refactor:#77 month optional

--- src/constants/query-keys.ts
+++ src/constants/query-keys.ts
@@ -1,5 +1,5 @@
 export const QUERY_KEY = {
   PROFILE: ['profile'],
   LIFE_POSTS_INFINITE: ['life-posts-infinite'],
-  LIFE_POSTS: (month: string) => ['life-posts', month]
+  LIFE_POSTS: (month?: string) => (month ? ['life-posts', month] : ['life-posts'])
 };
