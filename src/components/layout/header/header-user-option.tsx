03042b804f352b8962dac134e706d98fbdad4f07
SuimKim <rrrr6563@naver.com>
2025-04-11 09:51:42 +0900

    refactor:#99 프리패칭 로직 삭제

--- src/components/layout/header/header-user-option.tsx
+++ src/components/layout/header/header-user-option.tsx
@@ -1,26 +1,14 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import { useQueryClient } from '@tanstack/react-query';
 import clsx from 'clsx';
 import { User } from 'lucide-react';
-import { getUserProfile } from '@/lib/utils/api/auth-action';
 import HeaderDropdownMenu from '@/components/layout/header/header-dropdown-menu';
 import type { MenuItem } from '@/types/components/header';
 import { PATH } from '@/constants/page-path';
-import { QUERY_KEY } from '@/constants/query-keys';
 
 const UserOptionMenu = () => {
   const pathname = usePathname();
-  const queryClient = useQueryClient();
-
-  const handleMouseEnter = () => {
-    // 마우스 호버 시 프리패칭
-    queryClient.prefetchQuery({
-      queryKey: QUERY_KEY.PROFILE,
-      queryFn: getUserProfile
-    });
-  };
 
   const isSelect: boolean = pathname.includes(PATH.MYPAGE);
 
@@ -31,7 +19,6 @@ const UserOptionMenu = () => {
   return (
     <HeaderDropdownMenu menuItems={userMenuItem} isUserMenu>
       <div
-        onMouseEnter={handleMouseEnter}
         className={clsx(
           'flex size-[36px] items-center justify-center rounded-full hover:bg-amber-300',
           isSelect ? 'bg-amber-300' : 'bg-none'
