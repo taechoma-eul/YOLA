279fbae6f3db37354134786225f6992eb932859b
UrePu <darkin31313@gmail.com>
2025-04-11 10:13:39 +0900

    chore:#77 사용법 주석 추가

--- src/constants/query-keys.ts
+++ src/constants/query-keys.ts
@@ -3,3 +3,4 @@ export const QUERY_KEY = {
   LIFE_POSTS_INFINITE: ['life-posts-infinite'],
   LIFE_POSTS: (month?: string) => (month ? ['life-posts', month] : ['life-posts'])
 };
+// month 안쓸때 LIFE_POSTS()로 사용하고 month 쓸때는 LIFE_POSTS(month)로 사용
