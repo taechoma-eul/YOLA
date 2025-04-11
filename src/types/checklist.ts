6b338f9c3742f8a328d9b6182c767c41721eb25e
ye21iin <kathykr120@gmail.com>
2025-04-11 20:10:39 +0900

    refactor:#93 체크리스트 타입수정(gen-types)

--- src/types/checklist.ts
+++ src/types/checklist.ts
@@ -1,13 +1,14 @@
-import { Database, Tables } from './supabase';
+import { Enums, Tables } from '@/types/supabase';
 
 export type MissionType = Tables<'mission_list'>;
 
-export type Level = Database['public']['Enums']['level'];
-export type UserLevel = Database['public']['Tables']['user_level']['Row'];
+export type Level = Enums<'level'>;
+export type UserLevel = Tables<'user_level'>;
+
 export interface UserLevelByMissionType {
   userId: UserLevel['user_id'];
   decodedMission: string;
 }
 
-export type MissionMapType = Record<string, keyof Database['public']['Tables']['user_level']['Row']>;
+export type MissionMapType = Record<string, keyof UserLevel>;
 export type MissionTag = '혼밥' | '혼자여행' | '혼놀' | '청소' | '갓생';
