6c12e05581fa9542fbdecc53da52993c00a91664
smileeeeely <minjoole.ivy@gmail.com>
2025-04-11 19:53:59 +0900

    refactor:#19 서버 액션 -> 서버-서버 통신으로 변경 및 적용

--- src/lib/utils/api/missions.api.ts
+++ src/lib/utils/api/missions.api.ts
@@ -1,18 +1,23 @@
 import type { Tables } from '@/types/supabase';
-import { supabase } from '../supabase/supabase-client';
+import { createClient } from '@/lib/utils/supabase/supabase-server';
 import { TABLE } from '@/constants/supabase-tables-name';
 import { unstable_cache } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { PATH } from '@/constants/page-path';
 
+const GET_MISSIONS_DATA_KEY = 'mission-list-cache-key';
 // 미션리스트 전체 가져옴
-export const _getMissions = async (): Promise<Tables<'mission_list'>[]> => {
+export const _getMissionsData = async (): Promise<Tables<'mission_list'>[]> => {
+  const supabase = await createClient();
   const { data, error } = await supabase.from(TABLE.MISSION_LIST).select('*');
 
   if (error || !data) {
-    throw new Error('미션 가져오기 실패');
+    redirect(PATH.ERROR);
   }
   return data;
 };
 
-export const getMissions = unstable_cache(_getMissions, ['mission-list-cache-key'], {
+// next/cache 캐시 적용
+export const getMissionsData = unstable_cache(_getMissionsData, [GET_MISSIONS_DATA_KEY], {
   revalidate: false
 });
