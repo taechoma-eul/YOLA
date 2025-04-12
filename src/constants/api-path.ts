35fe178ba9c5759bb2d1661afae905d837720fa5
SuimKim <rrrr6563@naver.com>
2025-04-12 20:38:26 +0900

    refactor:#99 라우트 핸들러 경로 상수화

--- src/constants/api-path.ts
+++ src/constants/api-path.ts
@@ -4,5 +4,6 @@ export const API = {
   SOCIAL_LOGIN_CALL_BACK: '/api/auth/callback',
   GOOGLE_LOGIN: '/api/auth/google',
   KAKAO_LOGIN: '/api/auth/kakao',
-  DUPLICATE: '/api/auth/check-duplicate'
+  DUPLICATE: '/api/auth/check-duplicate',
+  PROFILE: '/api/auth/profile'
 };
--- src/lib/utils/api/auth-client.api.ts
+++ src/lib/utils/api/auth-client.api.ts
@@ -64,7 +64,7 @@ export const fetchDuplicateCheck = async (field: string, value: string): Promise
  */
 export const fetchUserProfile = async (): Promise<Tables<'users'>> => {
   try {
-    const res = await fetch(`${NEXT_SERVER_BASE_URL}/api/auth/profile`, {
+    const res = await fetch(`${NEXT_SERVER_BASE_URL}${API.PROFILE}`, {
       method: 'GET'
     });
 
