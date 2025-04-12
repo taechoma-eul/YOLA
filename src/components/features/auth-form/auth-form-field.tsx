365c8a80860f20d4d25f06d2ff3d829d6c1259a1
SuimKim <rrrr6563@naver.com>
2025-04-12 15:22:41 +0900

    refactor:#99 부모 컴포넌트 분리에 따른 리팩토링 - 중복체크 폼 아래에 뜨도록 변경 - 타입 변경 - 중복체크 set 방식 변경

--- src/components/features/auth-form/auth-form-field.tsx
+++ src/components/features/auth-form/auth-form-field.tsx
@@ -1,88 +1,98 @@
 'use client';
 
-import { Dispatch, SetStateAction, useEffect } from 'react';
-import { useFormContext, UseFormReturn } from 'react-hook-form';
-import { getDuplicateCheckData } from '@/lib/utils/api/auth-action';
-import { toastAlert } from '@/lib/utils/toast';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { ControllerRenderProps, FieldValues, useFormContext } from 'react-hook-form';
+import clsx from 'clsx';
+import { fetchDuplicateCheck } from '@/lib/utils/api/auth-client.api';
 import { Button } from '@/components/ui/button';
-import { FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import type { AuthFormData } from '@/lib/utils/validation/auth-validate';
-import { AUTH, LABEL } from '@/constants/auth-form';
-import { AUTH_ERROR, SUCCESS } from '@/constants/messages';
+import { FormControl, FormItem, FormMessage } from '@/components/ui/form';
+import DuplicateCheckMessage from '@/components/features/auth-form/duplicate-check-message';
+import { AUTH } from '@/constants/auth-form';
+import { AUTH_ERROR, FAIL, SUCCESS } from '@/constants/messages';
 
-interface FieldProps<T extends keyof AuthFormData> {
+interface FieldProps<T extends FieldValues> {
   inputType: string;
-  fieldName: T;
   placeholder: string;
   isCheckButton?: boolean;
-  form: UseFormReturn<AuthFormData, any, undefined>;
-  setNicknameDuplicateCheck: Dispatch<SetStateAction<boolean>>;
-  setEmailDuplicateCheck: Dispatch<SetStateAction<boolean>>;
+  fieldName: string;
+  field: ControllerRenderProps<T>;
+  isValid?: boolean;
+  setEmailDuplicateCheck?: Dispatch<SetStateAction<boolean>>;
+  setNicknameDuplicateCheck?: Dispatch<SetStateAction<boolean>>;
 }
 
-const AuthFormField = <T extends keyof AuthFormData>({
+const AuthFormField = <T extends FieldValues>({
   inputType,
-  fieldName,
   placeholder,
   isCheckButton = false,
-  form,
-  setNicknameDuplicateCheck,
-  setEmailDuplicateCheck
+  fieldName,
+  field,
+  isValid,
+  setEmailDuplicateCheck,
+  setNicknameDuplicateCheck
 }: FieldProps<T>) => {
+  const [errorMessage, setErrorMessage] = useState<string | null>(null); // 중복 확인 실패 메시지
+  const [successMessage, setSuccessMessage] = useState<string | null>(null); // 중복 확인 성공 메시지
   const { watch, getValues } = useFormContext();
   const fieldValue = watch(fieldName);
 
-  const setDuplicateCheck: Dispatch<SetStateAction<boolean>> =
-    fieldName === AUTH.EMAIL ? setEmailDuplicateCheck : setNicknameDuplicateCheck; // 필드에 따른 중복검사 체크를 위한 할당
+  const setDuplicateCheck = fieldName === AUTH.EMAIL ? setEmailDuplicateCheck : setNicknameDuplicateCheck; // 필드에 따른 중복검사 체크를 위한 할당
 
+  // -------------------------------------------------
   useEffect(() => {
-    setDuplicateCheck(false); // 값 변경 시 중복확인 상태 초기화\
-  }, [fieldValue, setDuplicateCheck]);
+    if (setDuplicateCheck && (fieldName === AUTH.EMAIL || fieldName === AUTH.NICKNAME)) {
+      setDuplicateCheck(false);
+      setSuccessMessage(null);
+      setErrorMessage(null);
+    }
+  }, [fieldValue, setDuplicateCheck, fieldName]);
+
+  // -------------------------------------------------
 
   const handleDuplicateCheck = async () => {
-    const nowValue: string = getValues(fieldName);
+    if (setDuplicateCheck) {
+      const nowValue: string = getValues(fieldName);
 
-    if (!nowValue) {
-      toastAlert(fieldName === AUTH.EMAIL ? AUTH_ERROR.NONE_EMAIL : AUTH_ERROR.NONE_NICKNAME, 'destructive');
-      return;
-    }
+      try {
+        const isDuplication = await fetchDuplicateCheck(fieldName, nowValue);
 
-    const data = await getDuplicateCheckData(fieldName, nowValue);
-
-    if (data) {
-      toastAlert(
-        fieldName === AUTH.EMAIL ? AUTH_ERROR.CHECK_EMAIL_FAIL : AUTH_ERROR.CHECK_NICKNAME_FAIL,
-        'destructive'
-      );
-    } else {
-      toastAlert(`${SUCCESS.CHECK} ${fieldName === AUTH.EMAIL ? LABEL.EMAIL : LABEL.NICKNAME}입니다.`, 'default');
-      setDuplicateCheck(true);
+        // 중복 있으면 리턴
+        if (isDuplication) {
+          setErrorMessage(fieldName === AUTH.EMAIL ? AUTH_ERROR.CHECK_EMAIL_FAIL : AUTH_ERROR.CHECK_NICKNAME_FAIL);
+          return;
+        }
+        setSuccessMessage(fieldName === AUTH.EMAIL ? SUCCESS.EMAIL_CHECK : SUCCESS.NICKNAME_CHECK);
+        setDuplicateCheck(true);
+        return;
+      } catch (error) {
+        setErrorMessage(FAIL.DUPLICATE);
+      }
     }
   };
 
   return (
-    <FormField
-      control={form.control}
-      name={fieldName}
-      render={({ field }) => (
-        <FormItem className="flex items-center space-x-5">
-          <div className="flex flex-1 items-center space-x-3">
-            <div className="flex-1">
-              <FormControl>
-                <Input className="h-11 w-full" placeholder={placeholder} type={inputType} {...field} />
-              </FormControl>
-              <FormMessage />
-            </div>
-            {isCheckButton && (
-              <Button type="button" className="h-11 w-[70px]" onClick={handleDuplicateCheck}>
-                중복확인
-              </Button>
-            )}
-          </div>
-        </FormItem>
-      )}
-    />
+    <FormItem className="flex items-center space-x-5">
+      <div className="flex flex-1 items-center space-x-3">
+        <div className="relative flex-1">
+          <FormControl>
+            <Input
+              className={clsx('h-11 w-full', errorMessage ? 'border-[#FF5E3A]' : 'border-stone-300')}
+              placeholder={placeholder}
+              type={inputType}
+              {...field}
+            />
+          </FormControl>
+          <FormMessage />
+          <DuplicateCheckMessage errorMessage={errorMessage} successMessage={successMessage} />
+        </div>
+        {isCheckButton && (
+          <Button disabled={!isValid} type="button" className="h-11 w-[70px]" onClick={handleDuplicateCheck}>
+            중복확인
+          </Button>
+        )}
+      </div>
+    </FormItem>
   );
 };
 
