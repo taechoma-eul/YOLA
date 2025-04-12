1b84ca3c365c95fa246100e821a9d1002ce3c100
SuimKim <rrrr6563@naver.com>
2025-04-12 23:43:30 +0900

    refactor:#104 체크리스트 목록 카드 컴포넌트 스타일 수정 및 이미지 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,34 @@
 import { getUserSessionState } from '@/lib/utils/api/auth-action';
-import { getUniqueMissionType } from '@/lib/utils/api/checklist.api';
 import CheckListCard from '@/components/features/home/main-checklist-card';
 import GonggamPreviewBox from '@/components/features/home/main-gonggam-preview';
 import MainBannerSwiper from '@/components/features/home/main-banner-swiper';
 import RandomMissionSection from '@/components/features/home/main-random-mission-section';
 
 const HomePage = async () => {
-  const checkListTypes = await getUniqueMissionType();
   const { isLogin } = await getUserSessionState();
 
+  const checkListTypes: string[] = ['혼밥', '혼자여행', '갓생', '청소', '혼놀'];
+
   return (
-    <div className="space-y-14">
+    <>
       <MainBannerSwiper />
-      <section className="space-y-5">
-        <strong className="text-xl">YOLA 챌린지</strong>
-        <div className="flex gap-10">
+      <section className="mt-14 w-[1280px] space-y-[26px] px-[40px]">
+        <strong className="justify-start self-stretch text-xl font-semibold leading-7 text-zinc-800">
+          YOLA 챌린지
+        </strong>
+        <div className="flex items-center justify-between self-stretch">
           {checkListTypes.map((item) => (
             <CheckListCard key={item} checkListType={item} />
           ))}
         </div>
       </section>
-      <hr className="mx-auto w-full max-w-[1280px] outline-neutral-300" />
-      <section className="relative grid max-w-[1280px] grid-cols-2 place-content-evenly items-center justify-items-center gap-4">
+      <hr className="mx-auto mt-[57px] w-full max-w-[1280px] outline-neutral-300" />
+      <section className="relative mt-[61px] grid max-w-[1280px] grid-cols-2 place-content-evenly items-center justify-items-center gap-4">
         <RandomMissionSection isLogin={isLogin} />
-        <div className="absolute left-[50%] h-72 w-0 outline outline-1 outline-neutral-200" />
+        <div className="absolute left-[50%] h-[237px] w-0 outline outline-1 outline-neutral-200" />
         <GonggamPreviewBox />
       </section>
-    </div>
+    </>
   );
 };
 
--- src/components/features/home/main-checklist-card.tsx
+++ src/components/features/home/main-checklist-card.tsx
@@ -1,18 +1,23 @@
 import Link from 'next/link';
-import { Image } from 'lucide-react';
+import Image from 'next/image';
 import { PATH } from '@/constants/page-path';
 
 const CheckListCard = ({ checkListType }: { checkListType: string }) => {
   return (
     <Link
       href={`${PATH.CHECKLIST}/${checkListType}`}
-      className="relative h-56 w-56 overflow-hidden rounded bg-white outline outline-1 outline-offset-[-1px] outline-stone-300"
+      className="relative h-56 w-56 overflow-hidden rounded-[30px] bg-white outline outline-1 outline-gray-300"
     >
-      <p>{checkListType}</p>
-      <p>체크리스트</p>
-      <div className="absolute left-0 top-[79px] h-36 w-56 overflow-hidden bg-neutral-200">
-        <Image className="absolute left-24 top-16" />
-        <div className="absolute left-[103px] top-[63px] h-4 w-4 overflow-hidden"></div>
+      <p className="absolute left-[16px] top-[19px] w-[70px] justify-start leading-snug text-zinc-800">
+        {checkListType} <br /> 체크리스트
+      </p>
+      <div className="absolute left-0 top-[79px] h-36 w-56 overflow-hidden bg-gray-50">
+        <Image
+          src={`/images/${checkListType}.svg`}
+          alt={`${checkListType} 체크리스트 이미지`}
+          width={221}
+          height={142}
+        />
       </div>
     </Link>
   );
