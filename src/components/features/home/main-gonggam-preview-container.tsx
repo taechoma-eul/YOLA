7cb296449bb04f61909962ce7c4df7705b824a02
SuimKim <rrrr6563@naver.com>
2025-04-13 18:11:16 +0900

    refactor:#104 임포트문 타입 명시

--- src/components/features/home/main-gonggam-preview-container.tsx
+++ src/components/features/home/main-gonggam-preview-container.tsx
@@ -1,4 +1,4 @@
-import { Children } from '@/types/children';
+import type { Children } from '@/types/children';
 
 const GonggamPreviewContainer = ({ children }: Children) => {
   return <div className="flex w-[578px] flex-col items-center justify-start gap-[16px]">{children}</div>;
