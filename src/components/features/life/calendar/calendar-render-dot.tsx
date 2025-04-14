243657aee450e7312fff9f7b0bf0cdcaa49e9196
UrePu <darkin31313@gmail.com>
2025-04-14 17:16:18 +0900

    refactor:#111 이름 재작성

--- src/components/features/life/calendar/calendar-render-dot.tsx
+++ src/components/features/life/calendar/calendar-render-dot.tsx
@@ -5,12 +5,12 @@ import React from 'react';
 export const renderDot = (
   dotMap: Record<string, Set<'mission' | 'normal'>>
 ): ((date: Date, isOutside: boolean) => React.ReactNode) => {
-  return (d, o) => {
-    const dateStr = format(d, 'yyyy-MM-dd');
+  return (date, isOutside) => {
+    const dateStr = format(date, 'yyyy-MM-dd');
     const dots = dotMap[dateStr];
-    const dotTextColor = o ? 'text-gray-300' : 'text-zinc-500';
-    const missionColor = o ? 'bg-rose-200' : 'bg-rose-400';
-    const normalColor = o ? 'bg-zinc-300' : 'bg-zinc-800';
+    const dotTextColor = isOutside ? 'text-gray-300' : 'text-zinc-500';
+    const missionColor = isOutside ? 'bg-rose-200' : 'bg-rose-400';
+    const normalColor = isOutside ? 'bg-zinc-300' : 'bg-zinc-800';
 
     return (
       <div className="flex h-14 w-full select-none flex-col items-center justify-start gap-1 overflow-hidden px-2.5 py-[3px]">
