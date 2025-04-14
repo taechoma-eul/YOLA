5d677087aa6bdfe0afad8aafaf93d64c4394e6a4
SuimKim <rrrr6563@naver.com>
2025-04-14 22:32:04 +0900

    fix: 버셀 배포시 api 경로 인식 못하는 문제 - base url 변수명 변경

--- src/constants/api-path.ts
+++ src/constants/api-path.ts
@@ -1,4 +1,4 @@
-export const NEXT_SERVER_BASE_URL = process.env.VERCEL_URL ?? 'http://localhost:3000'; // 버셀에선 배포 주소, 로컬에선 로컬 주소
+export const NEXT_SERVER_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'; // 버셀에선 배포 주소, 로컬에선 로컬 주소
 
 export const API = {
   SOCIAL_LOGIN_CALL_BACK: '/api/auth/callback',
