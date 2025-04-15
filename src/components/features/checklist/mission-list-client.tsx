b390f488112ddff02a246949475cdc72c33f01e9
ye21iin <kathykr120@gmail.com>
2025-04-16 00:41:10 +0900

    refactor:#140 미션 클릭 핸들러 및 메시지 상수 제거 - 추후 인증글 보러가기 연동을 위함

--- src/components/features/checklist/mission-list-client.tsx
+++ src/components/features/checklist/mission-list-client.tsx
@@ -1,10 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import { useToast } from '@/lib/hooks/use-toast';
 import { PATH } from '@/constants/page-path';
-import { MSG } from '@/constants/messages';
 import type { MissionType } from '@/types/checklist';
 import DONE from '@images/images/checklist-done.svg';
 import UNDO from '@images/images/checklist-undo.svg';
@@ -17,30 +14,6 @@ interface ClientMissionListProps {
 }
 
 const MissionListClient = ({ missionList, userId }: ClientMissionListProps) => {
-  const { toast } = useToast();
-  const router = useRouter();
-
-  const handleMissionClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    if (!userId) {
-      e.preventDefault();
-
-      toast({
-        title: MSG.NEED_LOGIN,
-        description: MSG.LOGIN_BEFORE_POST_MISSION_CLEAR,
-        variant: 'destructive'
-      });
-      router.push(PATH.LOGIN);
-    }
-  };
-
-  const handleCompletedMissionClick = () => {
-    toast({
-      title: MSG.ALREADY_CLEAR,
-      description: MSG.ALREADY_CLEAR_CHOOSE_OTHER,
-      variant: 'default'
-    });
-  };
-
   return (
     <ul className="mt-[129px] flex w-full max-w-[1200px] items-center gap-[24px]">
       {missionList.map((mission) => {
@@ -54,11 +27,9 @@ const MissionListClient = ({ missionList, userId }: ClientMissionListProps) => {
 
         const baseClasses = `h-[248px] w-[221px] rounded-[20px] border border-secondary-grey-300 bg-white flex flex-col items-start`;
 
-        const handleClick = isCompleted ? handleCompletedMissionClick : !userId ? handleMissionClick : undefined;
-
         return (
           <li key={mission.id} className="">
-            <Link href={href} onClick={handleClick} className={baseClasses}>
+            <Link href={href} className={baseClasses}>
               <strong className="flex w-[221px] items-start gap-[10px] px-[20px] py-[20px] pb-0 pt-[20px]">
                 <span className="flex-1 grow basis-0 text-[16px] font-semibold leading-[1.4] text-primary-orange-900">
                   {mission.content}
