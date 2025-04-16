7445efc5ad4893bdf19454185cab80bad57c1a70
ye21iin <kathykr120@gmail.com>
2025-04-17 00:47:36 +0900

    design:#153 글 작성 버튼 공통화

--- src/components/features/gonggam/gonggam-header.tsx
+++ src/components/features/gonggam/gonggam-header.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/button';
+import { CustomButton } from '@/components/ui/custom-button';
 import { PATH } from '@/constants/page-path';
 import Link from 'next/link';
 
@@ -7,7 +7,7 @@ const GonggamHeader = () => {
     <section className="mt-[44px] flex justify-between px-[10px] py-[8px]">
       <h1 className="py-[5px] text-center text-[20px] font-semibold leading-[140%]">혼자 라이프 공감 게시판</h1>
       <Link href={PATH.GONGGAM_POST}>
-        <Button>글 작성하기</Button>
+        <CustomButton size="gonggam-write">글 작성하기</CustomButton>
       </Link>
     </section>
   );
