6e642822e88c912d0ddfdaa0f0a2dbceadd7995b
Jieun Lee <2ur.leeje@gmail.com>
2025-04-16 20:15:49 +0900

    feat:#152 혼자라이프 모달 설치

--- src/components/features/mypage/my-life-list-client.tsx
+++ src/components/features/mypage/my-life-list-client.tsx
@@ -1,13 +1,17 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
-import SoloLifeCard from '@/components/common/solo-life-card';
 import useGetLifePostsInfiniteQuery from '@/lib/hooks/queries/use-get-life-posts-infinite-query';
+import SoloLifeCard from '@/components/common/solo-life-card';
+import { PostDetailModal } from '@/components/features/modals/calendar-post-detail';
 import { DEFAULT_LIFE_IMAGE_URL } from '@/constants/default-image-url';
-import type { GetLifePostsResponse, SoloLifeCardType } from '@/types/life-post';
+import type { GetLifePostsResponse, LifePostWithImageUrls, SoloLifeCardType } from '@/types/life-post';
 
 const MyLifeListClient = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedPost, setSelectedPost] = useState<LifePostWithImageUrls | null>(null);
+
   const {
     data: posts,
     isPending,
@@ -45,7 +49,18 @@ const MyLifeListClient = () => {
     ) ?? [];
 
   if (isPending) return <div className="p-4">로딩 중...</div>;
-  if (error) return <div className="p-4 text-red-500">에러 발생</div>;
+  if (error) throw error;
+
+  const handleClickCard = (id: string) => {
+    //posts가 InfiniteData로 되어 있어 flatMap 사용
+    const flatPosts = posts.pages.flatMap((page) => page.data) ?? [];
+    const post = flatPosts.find((post: LifePostWithImageUrls) => post.id === +id);
+    if (!post) {
+      return;
+    }
+    setSelectedPost(post);
+    setShowModal(true);
+  };
 
   return (
     <div className="grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
@@ -57,8 +72,11 @@ const MyLifeListClient = () => {
       ) : (
         <>
           {parsedList.map((post) => (
-            <SoloLifeCard key={post.id} {...post} />
+            <SoloLifeCard key={post.id} {...post} onClick={() => handleClickCard(post.id)} />
           ))}
+          {showModal && (
+            <PostDetailModal clickModal={() => setShowModal(false)} showModal={showModal} post={selectedPost} />
+          )}
 
           {/* 무한 스크롤 로딩 및 종료 메세지 */}
           <div className="col-span-full flex items-center justify-center py-4 text-sm text-slate-400">
