5e6e3789c43203c2a110980229c6d3ae5f2ea5d7
SuimKim <rrrr6563@naver.com>
2025-04-16 19:46:58 +0900

    design:#149 width 속성 삭제 및 align 속성 수정

--- src/components/features/home/main-gonggam-preview-title-section.tsx
+++ src/components/features/home/main-gonggam-preview-title-section.tsx
@@ -9,7 +9,7 @@ const TitleSection = () => {
         1인가구 공감 게시판
       </strong>
       <Link href={PATH.GONGGAM} className="flex h-[28px] items-center text-sm font-normal text-secondary-grey-900">
-        <p className="h-[17px] w-[49px]">전체보기</p>
+        <p className="h-[17px] text-end">전체보기</p>
         <Image
           src="images/gonggam-preview.svg"
           alt="공감게시판 전체보기 버튼"
