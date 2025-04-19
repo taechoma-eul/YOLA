13fd7867e3e5c90f75229c7dd5266b4647d399a8
SuimKim <rrrr6563@naver.com>
2025-04-20 03:36:11 +0900

    refactor:#187 미사용 객체 삭제 및 ul 태그 추가

--- src/components/layout/header/header-nav.tsx
+++ src/components/layout/header/header-nav.tsx
@@ -2,22 +2,14 @@ import NavChallengeMenu from '@/components/layout/header/header-nav-challenge-me
 import NavLinkItem from '@/components/layout/header/header-nav-link-item';
 import { PATH } from '@/constants/page-path';
 
-interface NavList {
-  href: string;
-  label: string;
-}
-
 const HeaderNav = () => {
-  const navList: NavList[] = [
-    { href: PATH.LIFE, label: '혼자라이프 달력' },
-    { href: PATH.GONGGAM, label: '공감 게시판' }
-  ];
-
   return (
-    <nav className="flex items-center justify-center gap-1">
-      <NavLinkItem label="혼자라이프 달력" href={PATH.LIFE} />
-      <NavLinkItem label="공감 게시판" href={PATH.GONGGAM} />
-      <NavChallengeMenu label="챌린지" />
+    <nav>
+      <ul className="flex items-center justify-center gap-[4px]">
+        <NavLinkItem label="혼자라이프 달력" href={PATH.LIFE} />
+        <NavLinkItem label="공감 게시판" href={PATH.GONGGAM} />
+        <NavChallengeMenu />
+      </ul>
     </nav>
   );
 };
