c8263419840cd1a0516eb6ed3244933cd321efad
SuimKim <rrrr6563@naver.com>
2025-04-20 03:50:56 +0900

    refactor:#187 드롭다운메뉴 내 link 태그와 구분선만 포함하는 컴포넌트로 리팩토링 - use client 선언 제거 - MenuItem 타입 단일 사용으로 직접 선언, 모듈 삭제 - isButton 조건부 렌더링 삭제로 인한 href!=='' 조건 삭제 - 상위 태그 link 로 변경, 요소 간소화

--- src/components/layout/header/header-dropdown-menu-item.tsx
+++ src/components/layout/header/header-dropdown-menu-item.tsx
@@ -1,44 +1,27 @@
-'use client';
-
-import { useQueryClient } from '@tanstack/react-query';
 import { clsx } from 'clsx';
 import Link from 'next/link';
-import { FAIL } from '@/constants/messages';
-import { logout } from '@/lib/utils/api/auth/auth-action';
-import { toastAlert } from '@/lib/utils/toast';
-import type { MenuItem } from '@/types/header';
 
-const HeaderDropdownMenuItem = ({ label, href, isLine = true, isButton = false, pathname }: MenuItem) => {
-  const queryClient = useQueryClient();
+interface MenuItem {
+  label: string;
+  href: string;
+  isLine?: boolean;
+  pathname?: string;
+}
 
+const HeaderDropdownMenuItem = ({ label, href, isLine = true, pathname }: MenuItem) => {
   const isSelect: boolean = decodeURIComponent(pathname!).includes(href);
-
-  const handleLogout = async () => {
-    try {
-      await logout();
-      queryClient.clear();
-    } catch {
-      toastAlert(FAIL.LOGOUT, 'default');
-      return;
-    }
-  };
 
   return (
     <>
-      <div
+      <Link
+        href={href}
         className={clsx(
           'flex h-[47px] items-center justify-center bg-white hover:text-primary-orange-600',
-          isSelect && href !== '' ? 'text-primary-orange-600' : 'text-secondary-grey-900'
+          isSelect ? 'text-primary-orange-600' : 'text-secondary-grey-900'
         )}
       >
-        {isButton ? (
-          <button className="text-secondary-grey-900 hover:text-primary-orange-600" onClick={handleLogout}>
-            {label}
-          </button>
-        ) : (
-          <Link href={href}>{label}</Link>
-        )}
-      </div>
+        {label}
+      </Link>
       {isLine && <div className="m-0 h-[1px] w-[60px] bg-secondary-grey-300" />}
     </>
   );
