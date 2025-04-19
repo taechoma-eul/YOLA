6a58bf5c1755d987042b82c3e0f29428db9f67b7
ye21iin <kathykr120@gmail.com>
2025-04-19 23:36:54 +0900

    fix:#172 linting error - 의존성배열 추가

--- src/components/features/gonggam/gonggam-likes.tsx
+++ src/components/features/gonggam/gonggam-likes.tsx
@@ -42,7 +42,7 @@ const GonggamLikes = ({ postId, userId }: GonggamLikesProps) => {
       }
     };
     fetchLikeStatus();
-  }, []);
+  }, [postId, userId]);
 
   const handleLikeToggle = async () => {
     if (!userId) {
