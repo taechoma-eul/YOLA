700e5f94d915c69139b8f817404c405d9ec1c52b
SuimKim <rrrr6563@naver.com>
2025-04-20 16:13:39 +0900

    refactor:#110 푸터 접근성 부분 개선 작업 - 로고 및 텍스트, 배경색 변경

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -3,11 +3,11 @@ import { FOOTER_LOGO_URL } from '@/constants/default-image-url';
 
 const Footer = () => {
   return (
-    <div className="flex h-36 w-full flex-col items-center justify-center justify-items-center overflow-hidden bg-gray-50">
+    <div className="bg-secondary-grey-150 flex h-36 w-full flex-col items-center justify-center justify-items-center overflow-hidden">
       <div className="mx-auto flex h-[44px] w-[196px] items-center justify-center gap-2.5 self-stretch">
         <Image src={FOOTER_LOGO_URL} alt="푸터 로고" width={84} height={23} />
       </div>
-      <p className="justify-start self-stretch text-center text-xs font-normal leading-none text-neutral-400">
+      <p className="justify-start self-stretch text-center text-xs font-normal leading-none text-secondary-grey-800">
         ⓒ 2025. 태초마을 all rights reserved.
       </p>
     </div>
