e9338a4e6dc45e0e68a0ccb250346e4a3d26e4b6
Jieun Lee <2ur.leeje@gmail.com>
2025-04-21 22:17:31 +0900

    refactor:#206 props 리팩토링 및 mode 적용 (모바일, 데스크탑)

--- src/components/features/mypage/profile-avatar.tsx
+++ src/components/features/mypage/profile-avatar.tsx
@@ -1,13 +1,29 @@
 import Image from 'next/image';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { DEFAULT_AVATAR_URL } from '@/constants/default-image-url';
+import { clsx } from 'clsx';
 
-const ProfileAvatar = ({ src }: { src: string | null }) => {
+interface ProfileAvatarProps {
+  src: string | null;
+  mode: 'mobile' | 'pc';
+}
+
+const ProfileAvatar = ({ src, mode }: ProfileAvatarProps) => {
   return (
-    <Avatar className="mb-[20px] h-[172px] w-[172px] rounded-2xl border-2 border-[#EFF1F3]">
+    <Avatar
+      className={clsx('mb-[20px] rounded-2xl border-2 border-[#EFF1F3]', {
+        'min-h-[48px] min-w-[48px]': mode === 'mobile',
+        'h-[172px] w-[172px]': mode === 'pc'
+      })}
+    >
       <AvatarImage src={src ? src : ''} />
       <AvatarFallback className="rounded-2xl">
-        <Image src={DEFAULT_AVATAR_URL} alt="디폴트 프로필 이미지" width={172} height={172} />
+        <Image
+          src={DEFAULT_AVATAR_URL}
+          alt="디폴트 프로필 이미지"
+          width={mode === 'mobile' ? 48 : 172}
+          height={mode === 'mobile' ? 48 : 172}
+        />
       </AvatarFallback>
     </Avatar>
   );
