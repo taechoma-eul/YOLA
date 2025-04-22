9b52fec697289610f497b41fbf34afd88e328c85
Jieun Lee <2ur.leeje@gmail.com>
2025-04-22 19:17:51 +0900

    refactor:#206 profile-avatar 코드 원상복구

--- src/components/features/mypage/edit-profile-form-image-field.tsx
+++ src/components/features/mypage/edit-profile-form-image-field.tsx
@@ -45,7 +45,7 @@ const ProfileImageField = ({
       name={AUTH.PROFILE_IMAGE}
       render={() => (
         <div className="relative flex w-44 flex-col items-end justify-start">
-          <ProfileAvatar src={previewUrl ? previewUrl : profileImage} size={172} className="rounded-2xl" />
+          <ProfileAvatar src={previewUrl ? previewUrl : profileImage} mode="desktop" />
           <FormControl>
             <Input type="file" className="hidden" ref={fileInputRef} onChange={handleFileChange} />
           </FormControl>
