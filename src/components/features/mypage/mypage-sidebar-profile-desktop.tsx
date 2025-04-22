969b63ebc23447dfff305a58907b818c00025601
Jieun Lee <2ur.leeje@gmail.com>
2025-04-22 16:25:39 +0900

    design:#206 마이페이지 프로필 사진 밑에 마진 20px

--- src/components/features/mypage/mypage-sidebar-profile-desktop.tsx
+++ src/components/features/mypage/mypage-sidebar-profile-desktop.tsx
@@ -19,7 +19,7 @@ const ProfileBoxDesktop = ({ initProfile }: InitProfile) => {
     <section className="flex flex-col items-center justify-start">
       {/* 프로필 영역 */}
       <ProfileAvatar src={profile.profile_image} size={172} className="rounded-2xl" />
-      <p className="text-xl font-semibold text-secondary-grey-900">{profile.nickname}</p>
+      <p className="mt-[20px] text-xl font-semibold text-secondary-grey-900">{profile.nickname}</p>
       <p className="mb-[20px] mt-[8px] text-base text-secondary-grey-700">{profile.email}</p>
 
       {/* 프로필 수정 버튼 */}
