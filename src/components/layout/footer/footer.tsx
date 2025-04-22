bd1b36a1b043fc31a03ce266663e1efa2611933b
SuimKim <rrrr6563@naver.com>
2025-04-22 12:39:00 +0900

    refactor:#110 이미지 임포트 방식 변경

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
-import { FOOTER_LOGO_URL } from '@/constants/default-image-url';
+import FOOTER_LOGO from '@images/images/footer-logo.svg';
 
 const Footer = () => {
   return (
-    <div className="bg-secondary-grey-150 flex h-36 w-full flex-col items-center justify-center justify-items-center overflow-hidden">
+    <div className="flex h-36 w-full flex-col items-center justify-center justify-items-center overflow-hidden bg-secondary-grey-150">
       <div className="mx-auto flex h-[44px] w-[196px] items-center justify-center gap-2.5 self-stretch">
-        <Image src={FOOTER_LOGO_URL} alt="푸터 로고" width={84} height={23} />
+        <Image src={FOOTER_LOGO} alt="푸터 로고" width={84} height={23} />
       </div>
       <p className="justify-start self-stretch text-center text-xs font-normal leading-none text-secondary-grey-800">
         ⓒ 2025. 태초마을 all rights reserved.
