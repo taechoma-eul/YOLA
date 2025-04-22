8c1f18516c061b3cf3d4735b90f06bda2a857c7d
Jieun Lee <2ur.leeje@gmail.com>
2025-04-22 15:39:37 +0900

    fix:#206 고양이 이미지 console warning 해결

--- src/components/features/mypage/mypage-no-records-box.tsx
+++ src/components/features/mypage/mypage-no-records-box.tsx
@@ -8,13 +8,9 @@ interface NoRecordsBoxProps {
 const NoRecordsBox = ({ mode }: NoRecordsBoxProps) => {
   return (
     <div className="col-span-full mb-[272px] flex min-h-[542px] flex-col items-center justify-center whitespace-normal rounded-[12px] bg-secondary-grey-100 text-center text-secondary-grey-500">
-      <Image
-        src={CAT_NO_RECORDS}
-        alt="기록없음을 알리는 고양이 일러스트"
-        width={65}
-        height={58}
-        className="mb-[20px]"
-      />
+      <figure className="relative mb-[20px] h-[58px] w-[65px]">
+        <Image src={CAT_NO_RECORDS} alt="기록없음을 알리는 고양이 일러스트" fill className="object-contain" />
+      </figure>
       <p>앗 아직 기록이 없어요</p>
       <p>{mode === '공감' ? '공감 게시판에서 글을 작성해보세요!' : '오늘의 혼자 라이프를 기록해보세요!'}</p>
     </div>
