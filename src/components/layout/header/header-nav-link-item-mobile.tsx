0a9084be7c90dfce5651771eb2f9d0c1ce2bb21a
SuimKim <rrrr6563@naver.com>
2025-04-23 01:03:42 +0900

    refactor:#220 해당 페이지 접속 중 메뉴 배경색 변경을 위한 로직 및 조건부 스타일링 추가

--- src/components/layout/header/header-nav-link-item-mobile.tsx
+++ src/components/layout/header/header-nav-link-item-mobile.tsx
@@ -1,12 +1,29 @@
+'use client';
+
+import { clsx } from 'clsx';
 import Link from 'next/link';
 import NavLabel from '@/components/layout/header/header-nav-label';
 import { SheetClose } from '@/components/ui/sheet';
+import { usePathnameContext } from '@/contexts/pathname-context';
 
-const NavMobileLinkItem = ({ href, label }: { href: string; label: string }) => {
+interface ItemProps {
+  href: string;
+  label: string;
+  isDropDown?: boolean;
+}
+
+const NavMobileLinkItem = ({ href, label, isDropDown = false }: ItemProps) => {
+  const { pathname } = usePathnameContext();
+
+  const isSelect: boolean = decodeURIComponent(pathname).includes(href);
+
   return (
     <li className="w-full justify-start leading-snug hover:bg-primary-orange-200">
       <SheetClose asChild>
-        <Link href={href} className="block px-5 py-4">
+        <Link
+          href={href}
+          className={clsx('block py-4', isDropDown ? 'px-[30px]' : 'px-5', isSelect && 'bg-primary-orange-200')}
+        >
           <NavLabel label={label} isMobile />
         </Link>
       </SheetClose>
