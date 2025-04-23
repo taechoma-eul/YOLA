ef4816496870c8d20dec3b2040498482849e070b
SuimKim <rrrr6563@naver.com>
2025-04-24 00:14:09 +0900

    design:#247 스타일 조정

--- src/app/(auth)/mypage/page.tsx
+++ src/app/(auth)/mypage/page.tsx
@@ -7,7 +7,9 @@ const MyPage = async () => {
 
   return (
     <>
-      <h1 className="mb-[23px] ml-[44px] mt-[72px] text-xl font-bold">프로필 수정</h1>
+      <h1 className="mb-[24px] mt-[72px] self-stretch text-xl font-semibold leading-7 text-secondary-grey-900">
+        프로필 수정
+      </h1>
       <EditProfileForm initProfile={profile} />
     </>
   );
