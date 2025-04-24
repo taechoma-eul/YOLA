90e33369694ee333b7706e8efcd70ec1986800a4
SuimKim <rrrr6563@naver.com>
2025-04-24 17:18:30 +0900

    refactor:#253 text block 컴포넌트 분리

--- src/components/features/onboarding/challenge-section.tsx
+++ src/components/features/onboarding/challenge-section.tsx
@@ -1,15 +1,11 @@
 import Image from 'next/image';
 import CHECKLIST_CARD from '@images/images/onboarding-checklist-card.svg';
+import TextBlock from '@/components/features/onboarding/text-block';
 
 const ChallengeSection = () => {
   return (
-    <section className="flex w-full flex-col items-center justify-center px-10 py-[90px]">
-      <div className="mb-[45px] inline-flex flex-col items-center justify-start gap-4 self-stretch text-center text-lg">
-        <strong className="h-[25px] font-semibold leading-relaxed text-primary-orange-700">YOLA 챌린지</strong>
-        <p className="h-[25px] justify-start self-stretch text-center text-lg leading-relaxed text-secondary-grey-900">
-          혼자만의 여유 시간에 즐길 수 있는 다양한 챌린지들이 준비되어 있어요
-        </p>
-      </div>
+    <section className="flex w-full flex-col items-center px-10 py-[90px]">
+      <TextBlock title="YOLA 챌린지">혼자만의 여유 시간에 즐길 수 있는 다양한 챌린지들이 준비되어 있어요</TextBlock>
       <Image src={CHECKLIST_CARD} alt="체크리스트 카드 미리보기" width={715} height={454} />
     </section>
   );
