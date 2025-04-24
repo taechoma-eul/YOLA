fe6cdcbd6610a58b57bcbea66955691af8bd38aa
Jieun Lee <2ur.leeje@gmail.com>
2025-04-24 21:07:07 +0900

    Feature:#254 기능 구현을 위한 코드 리팩토링 및 통일

--- src/app/(auth)/mypage/my-life-list/page.tsx
+++ src/app/(auth)/mypage/my-life-list/page.tsx
@@ -2,17 +2,12 @@ import MyLifeListClient from '@/components/features/mypage/my-life-list-client';
 import { getUserProfile } from '@/lib/utils/api/auth/auth.api';
 
 const MyLifeListPage = async () => {
+  //유저 닉네임 조회
   const profile = await getUserProfile();
   if (!profile) throw new Error();
+  const nickname = profile.nickname;
 
-  return (
-    <section className="mt-[20px] px-[16px] md:mt-[72px]">
-      <h2 className="mb-[35px] hidden justify-start self-stretch text-xl font-semibold leading-7 text-secondary-grey-900 md:block">
-        {profile.nickname}님의 혼자 라이프 기록
-      </h2>
-      <MyLifeListClient />
-    </section>
-  );
+  return <MyLifeListClient nickname={nickname} />;
 };
 
 export default MyLifeListPage;
