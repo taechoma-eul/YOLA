d18e2f188f414931e60ced325f828e3e585bced4
SuimKim <rrrr6563@naver.com>
2025-04-25 12:36:38 +0900

    refactor:#253 이미지 드래그 방지 속성 추가

--- src/components/features/onboarding/mission-section.tsx
+++ src/components/features/onboarding/mission-section.tsx
@@ -5,7 +5,7 @@ import LEVEL_PREVIEW from '@images/images/onboarding-level.svg';
 
 const MissionSection = () => {
   return (
-    <section className="flex w-full flex-col items-center bg-secondary-grey-100 px-10 py-[90px]">
+    <section className="flex w-full flex-col items-center bg-secondary-grey-100 py-[90px]">
       <TextBlock title="챌린지 인증하기">
         미션을 인증했다면, 인증글을 작성해 레벨을 높일 수 있어요. <br />
         챌린지 탭에서 바로 인증글을 작성해 보세요!
@@ -16,12 +16,20 @@ const MissionSection = () => {
         width={548}
         height={288}
         className="mb-20 mt-10"
+        draggable="false"
       />
       <TextBlock title="레벨 시스템">
         챌린지별로 레벨을 확인할 수 있어요. <br />
         다양한 미션을 수행하며 레벨을 높여봐요!
       </TextBlock>
-      <Image src={LEVEL_PREVIEW} alt="레벨 시스템 미리보기" width={548} height={288} className="mt-10" />
+      <Image
+        src={LEVEL_PREVIEW}
+        alt="레벨 시스템 미리보기"
+        width={548}
+        height={288}
+        className="mt-10"
+        draggable="false"
+      />
     </section>
   );
 };
--- src/components/features/onboarding/title-section.tsx
+++ src/components/features/onboarding/title-section.tsx
@@ -3,6 +3,8 @@ import TITLE from '@images/images/onboarding-title.svg';
 import CAT from '@images/images/onboarding-cat.svg';
 import SLIDE from '@images/images/onboarding-slide-icon.svg';
 import { CustomButton } from '@/components/ui/custom-button';
+import Link from 'next/link';
+import { PATH } from '@/constants/page-path';
 
 const TitleSection = () => {
   return (
@@ -10,18 +12,23 @@ const TitleSection = () => {
       aria-label="타이틀 소개 섹션"
       className="relative flex h-[733px] w-full flex-col items-center justify-center bg-primary-orange-50"
     >
-      <Image src={TITLE} alt="온보딩 페이지 타이틀: 혼자서도 즐겁게!" width={368} height={48} />
+      <Image src={TITLE} alt="온보딩 페이지 타이틀: 혼자서도 즐겁게!" width={388} height={48} draggable="false" />
       <p className="mb-[80px] mt-[32px] text-center">
         YOLA는 혼자만의 하루를 의미 있고 즐겁게 만드는 라이프 기록 서비스예요.
         <br /> 일상을 가볍게 기록하고, 작은 도전으로 혼자 라이프를 즐겨보세요!
       </p>
-      <CustomButton>바로 시작하기</CustomButton>
+      <CustomButton asChild>
+        <Link href={PATH.HOME} aria-label="메인 페이지로 이동하기 버튼">
+          바로 시작하기
+        </Link>
+      </CustomButton>
       <Image
         src={CAT}
         alt="온보딩 페이지의 타이틀을 소개하는 고양이 캐릭터"
         width={331}
         height={306}
         className="absolute bottom-0 right-10"
+        draggable="false"
       />
       <Image
         src={SLIDE}
@@ -29,6 +36,7 @@ const TitleSection = () => {
         width={66}
         height={48}
         className="absolute bottom-[34px] right-[607px]"
+        draggable="false"
       />
     </section>
   );
