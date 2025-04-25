c47b48001cfb41c29b96e3b5d39b6cd44c77cfa3
SuimKim <rrrr6563@naver.com>
2025-04-25 12:47:20 +0900

    refactor:#253 푸터에 온보딩 페이지 이동 옵션 추가

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,13 +1,22 @@
 import Image from 'next/image';
 import FOOTER_LOGO from '@images/images/footer-logo.svg';
+import Link from 'next/link';
+import { PATH } from '@/constants/page-path';
 
 const Footer = () => {
   return (
-    <div className="flex h-[150px] w-full flex-col items-center justify-center justify-items-center overflow-hidden bg-secondary-grey-150">
+    <div className="flex h-[150px] w-full flex-col items-center justify-center justify-items-center overflow-hidden bg-secondary-grey-150 text-xs">
       <div className="mx-auto flex h-[44px] items-center justify-center">
         <Image src={FOOTER_LOGO} alt="푸터 로고" width={84} height={23} />
       </div>
-      <p className="mt-[2px] justify-start self-stretch text-center text-xs leading-tight text-secondary-grey-800">
+      <Link
+        aria-label="YOLA 소개 페이지로 이동하기"
+        href={PATH.ONBOARDING}
+        className="flex h-[29px] items-center justify-center leading-none text-secondary-grey-900 underline"
+      >
+        YOLA 소개
+      </Link>
+      <p className="justify-start self-stretch text-center leading-tight text-secondary-grey-800">
         ⓒ 2025. 태초마을 all rights reserved.
       </p>
     </div>
