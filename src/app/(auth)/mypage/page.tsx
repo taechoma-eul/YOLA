a51a4ad082779b9f264e96587ed7a2a891225de2
SuimKim <rrrr6563@naver.com>
2025-04-25 15:26:17 +0900

    refactor:#247 변경된 뒤로가기 버튼 컴포넌트 적용

--- src/app/(auth)/mypage/page.tsx
+++ src/app/(auth)/mypage/page.tsx
@@ -1,23 +1,14 @@
 import EditProfileForm from '@/components/features/mypage/edit-profile-form';
-import { PATH } from '@/constants/page-path';
+import BackButton from '@/components/features/mypage/edit-profile-mobile-back-button';
 import { getUserProfile } from '@/lib/utils/api/auth/auth.api';
-import Image from 'next/image';
-import Link from 'next/link';
-import BACK_ICON from '@images/images/go-back-icon.svg';
 
 const MyPage = async () => {
   const profile = await getUserProfile();
   if (!profile) throw Error;
 
   return (
-    <div className="fixed inset-0 z-20 min-h-screen w-full overflow-hidden bg-white md:static md:z-0 md:min-h-0">
-      <Link
-        href={PATH.MY_ACHIEVEMENT}
-        aria-label="나의 달성도 페이지로 돌아가기 버튼"
-        className="absolute left-[26px] top-[18px] h-6 w-6 md:hidden"
-      >
-        <Image src={BACK_ICON} alt="나의 달성도 페이지로 돌아가기 버튼 아이콘" width={16} height={24} />
-      </Link>
+    <div className="absolute inset-0 z-20 min-h-screen w-full bg-white md:static md:z-0 md:min-h-0">
+      <BackButton />
       <h1 className="mx-auto mt-[17.5px] h-[25px] w-[255px] self-stretch text-center text-lg font-semibold leading-7 text-secondary-grey-900 md:mb-[24px] md:mt-[72px] md:h-[28px] md:w-full md:text-left md:text-xl">
         프로필 수정
       </h1>
