9ba3b083e0833ba420bc90a013443b4de54b1c09
Jieun Lee <2ur.leeje@gmail.com>
2025-04-25 15:15:59 +0900

    refactor:#254 에러 메세지 상수화 적용

--- src/app/(auth)/mypage/my-gonggam-post-list/page.tsx
+++ src/app/(auth)/mypage/my-gonggam-post-list/page.tsx
@@ -1,10 +1,11 @@
 import MyGonggamPostClient from '@/components/features/mypage/my-gonggam-post-client';
+import { FAIL } from '@/constants/messages';
 import { getUserProfile } from '@/lib/utils/api/auth/auth.api';
 
 const MyGonggamPostListPage = async () => {
   //유저 닉네임 조회
   const profile = await getUserProfile();
-  if (!profile) throw new Error('유저의 닉네임 정보를 불러오지 못했습니다');
+  if (!profile) throw new Error(FAIL.GET_PROFILE);
   const nickname = profile.nickname;
   return <MyGonggamPostClient nickname={nickname} />;
 };
--- src/app/(auth)/mypage/my-life-list/page.tsx
+++ src/app/(auth)/mypage/my-life-list/page.tsx
@@ -1,10 +1,11 @@
 import MyLifeListClient from '@/components/features/mypage/my-life-list-client';
+import { FAIL } from '@/constants/messages';
 import { getUserProfile } from '@/lib/utils/api/auth/auth.api';
 
 const MyLifeListPage = async () => {
   //유저 닉네임 조회
   const profile = await getUserProfile();
-  if (!profile) throw new Error('유저의 닉네임 정보를 불러오지 못했습니다');
+  if (!profile) throw new Error(FAIL.GET_PROFILE);
   const nickname = profile.nickname;
 
   return <MyLifeListClient nickname={nickname} />;
