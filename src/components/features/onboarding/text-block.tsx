0b490b0b6b576f667ab7b5950100a7ca474021d7
SuimKim <rrrr6563@naver.com>
2025-04-25 12:36:19 +0900

    design:#253 스타일 조정

--- src/components/features/onboarding/text-block.tsx
+++ src/components/features/onboarding/text-block.tsx
@@ -2,7 +2,7 @@ import { Children } from '@/types/children';
 
 const TextBlock = ({ title, children }: { title: string } & Children) => {
   return (
-    <div className="mb-[45px] inline-flex flex-col items-center justify-start gap-4 self-stretch text-center text-lg">
+    <div className="inline-flex flex-col items-center justify-start gap-4 self-stretch text-center text-lg">
       <strong className="h-[25px] font-semibold leading-relaxed text-primary-orange-700">{title}</strong>
       <p className="justify-start self-stretch text-center text-lg leading-[1.39] text-secondary-grey-900">
         {children}
