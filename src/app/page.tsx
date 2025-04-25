4f4237eabb98858588b76f0e3ce5c8b6d8e7723c
SuimKim <rrrr6563@naver.com>
2025-04-25 12:35:32 +0900

    feat:#253 루트 페이지에 온보딩 컴포넌트 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
+import ChallengeSection from '@/components/features/onboarding/challenge-section';
+import EndSection from '@/components/features/onboarding/end-section';
+import FloatingButton from '@/components/features/onboarding/floating-button';
+import GonggamBoardSection from '@/components/features/onboarding/gonggam-board-section';
+import LifeSection from '@/components/features/onboarding/life-section';
+import MissionSection from '@/components/features/onboarding/mission-section';
+import RandomMissionSection from '@/components/features/onboarding/random-mission-section';
+import TitleSection from '@/components/features/onboarding/title-section';
+
 const OnboardingPage = () => {
-  return <div>OnboardingPage</div>;
+  return (
+    <>
+      <TitleSection />
+      <ChallengeSection />
+      <MissionSection />
+      <LifeSection />
+      <RandomMissionSection />
+      <GonggamBoardSection />
+      <EndSection />
+      <FloatingButton />
+    </>
+  );
 };
 
 export default OnboardingPage;
