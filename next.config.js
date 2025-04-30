c117cf47fd0ba46772bb3c3ca821eb985cda0385
SuimKim <rrrr6563@naver.com>
2025-04-30 20:58:24 +0900

    refactor:#289 이미지 허용 도메인 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
 const nextConfig = {
   images: {
-    domains: ['rrrswimuyqumlnkrfsli.supabase.co', 'k.kakaocdn.net', 'lh3.googleusercontent.com']
+    domains: ['rrrswimuyqumlnkrfsli.supabase.co', 'k.kakaocdn.net', 'lh3.googleusercontent.com', 'img1.kakaocdn.net']
   }
 };
 
