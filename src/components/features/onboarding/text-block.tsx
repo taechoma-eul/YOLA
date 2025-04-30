11bc5172b88b1fa77c1e23c3906eea54490f9cd3
SuimKim <rrrr6563@naver.com>
2025-05-01 00:38:17 +0900

    design:#263 challenge section 모바일 시안 반영

--- src/components/features/onboarding/text-block.tsx
+++ src/components/features/onboarding/text-block.tsx
@@ -1,10 +1,24 @@
+import { clsx } from 'clsx';
 import type { Children } from '@/types/children';
 
-const TextBlock = ({ title, children }: { title: string } & Children) => {
+interface TextBlockProps extends Children {
+  title: string;
+  className: string;
+  isMobile?: boolean;
+}
+
+const TextBlock = ({ title, children, className, isMobile = false }: TextBlockProps) => {
   return (
-    <div className="inline-flex flex-col items-center justify-start gap-4 self-stretch text-center text-lg">
+    <div
+      className={`inline-flex flex-col items-center justify-start gap-4 self-stretch text-center text-lg ${className}`}
+    >
       <strong className="h-[25px] font-semibold leading-relaxed text-primary-orange-700">{title}</strong>
-      <p className="justify-start self-stretch text-center text-lg leading-[1.39] text-secondary-grey-900">
+      <p
+        className={clsx(
+          'justify-start self-stretch text-center text-lg leading-[1.39] text-secondary-grey-900',
+          isMobile && 'h-[44px]'
+        )}
+      >
         {children}
       </p>
     </div>
