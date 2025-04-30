eb15bf0b8041f8d7c10190327cbcc53790aa9894
SuimKim <rrrr6563@naver.com>
2025-05-01 02:20:46 +0900

    refactor:#263 end section 모바일 디자인 반영

--- src/components/features/onboarding/end-section.tsx
+++ src/components/features/onboarding/end-section.tsx
@@ -4,12 +4,14 @@ import { PATH } from '@/constants/page-path';
 
 const EndSection = () => {
   return (
-    <section className="flex w-full flex-col items-center gap-[50px] bg-gradient-to-b from-white to-[#FFF8E3] py-[245px]">
-      <div className="flex flex-col items-center justify-start gap-2 self-stretch text-center">
-        <strong className="h-[42px] text-3xl font-semibold leading-10 text-primary-orange-600">
+    <section className="flex w-full flex-col items-center gap-10 bg-gradient-to-b from-white to-[#FFF8E3] py-[60px] md:gap-[50px] md:py-[245px]">
+      <div className="flex flex-col items-center justify-start gap-1 self-stretch text-center md:gap-2">
+        <strong className="h-[29px] text-xl font-semibold leading-7 text-primary-orange-600 md:h-[42px] md:text-3xl md:leading-10">
           이젠, YOLA와 함께
         </strong>
-        <p className="h-[25px] text-lg leading-relaxed">혼자서도 즐거운 일상을 시작해 보세요!</p>
+        <p className="text-base leading-snug md:h-[25px] md:text-lg md:leading-relaxed">
+          혼자서도 즐거운 일상을 시작해 보세요!
+        </p>
       </div>
       <CustomButton asChild>
         <Link href={PATH.HOME} aria-label="메인 페이지로 이동하기 버튼">
