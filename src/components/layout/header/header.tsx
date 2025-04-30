3e7750103776d1e207782c27be7b96f03f6b4f57
SuimKim <rrrr6563@naver.com>
2025-04-30 20:05:26 +0900

    refactor:#289 헤더 레이아웃 시프트 개선

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -5,7 +5,7 @@ const Header = async () => {
   const { isLogin } = await getUserSessionState();
 
   return (
-    <header className="fixed left-0 top-0 z-10 inline-flex h-[60px] w-full flex-col justify-center bg-white outline outline-1 outline-offset-[-1px] outline-secondary-grey-200 md:h-[100px]">
+    <header className="sticky left-0 top-0 z-10 inline-flex w-full flex-col justify-center bg-white outline outline-1 outline-offset-[-1px] outline-secondary-grey-200 md:h-[100px]">
       <div className="mx-auto flex w-full max-w-[1280px] items-center justify-between px-4 md:px-10">
         <HeaderNav isLogin={isLogin} />
       </div>
